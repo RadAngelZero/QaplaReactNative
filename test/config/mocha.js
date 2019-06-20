13282c54a8d40b3107b3f4fe57ce93b6135e37b2
DHVSGG <diego@qapla.gg>
2019-06-20 17:09:20 -0500

    Method to avoid error with Image on testing added

--- test/config/mocha.js
+++ test/config/mocha.js
@@ -3,6 +3,17 @@ const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const m = require('module');
+const originalLoader = m._load;
+
+m._load = function hookedLoader(request, parent, isMain) {
+  if (request.match(/.jpeg|.jpg|.png$/)) {
+    return { uri: request };
+  }
+
+  return originalLoader(request, parent, isMain);
+};
+
 require('react-native-mock-render/mock');
 
 require('@babel/register')({

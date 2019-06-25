eb7dddc90d3e430b6ff3b9c34110b6da75b029b2
DHVSGG <diego@qapla.gg>
2019-06-25 16:54:30 -0500

    Update to show in correct way the data on retas list

--- src/components/MatchCard/MatchCardList.js
+++ src/components/MatchCard/MatchCardList.js
@@ -5,11 +5,12 @@ import { styles } from './style';
 
 class MatchCardList extends Component {
     render() {
-        const reversedUserNamesArray = this.props.userNames.reverse();
+        const reversedUserNamesArray = [...this.props.userNames].reverse();
+        const reversedMatchesArray = [...this.props.matches].reverse();
         return (
             <View style={styles.listContainer}>
                 {this.props.userNames.length === this.props.matches.length &&
-                    <FlatList data={this.props.matches.reverse()}
+                    <FlatList data={reversedMatchesArray}
                         renderItem={({item, index}) => <MatchCardItem key={item.alphaNumericIdMatch} userName={reversedUserNamesArray[index]} {...item} />}
                         keyExtractor={(item) => item.alphaNumericIdMatch} />
                 }

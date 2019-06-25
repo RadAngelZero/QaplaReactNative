41e108b56039299ce5b52239c17ea388b84863f2
DHVSGG <diego@qapla.gg>
2019-06-25 16:53:32 -0500

    MockScreens created

---
+++ src/screens/MockScreen1/MockScreen1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class MockScreen1 extends Component {
+    render() {
+        return (
+            <Text>Mock 1</Text>
+        );
+    }
+}
+
+export default MockScreen1;
---
+++ src/screens/MockScreen2/MockScreen2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class MockScreen2 extends Component {
+    render() {
+        return (
+            <Text>Mock 2</Text>
+        );
+    }
+}
+
+export default MockScreen2;

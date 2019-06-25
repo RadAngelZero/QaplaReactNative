f8c8c6d44f648049e2ed8f229efd966ce0855f7c
DHVSGG <diego@qapla.gg>
2019-06-25 16:53:17 -0500

    Router implemented on App.js

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HeaderBar from './src/components/HeaderBar/HeaderBar';
+import Router from './src/Router';
+
+console.disableYellowBox = true;
 
 export default class App extends Component {
   render() {
     return (
-      <>
-        <HeaderBar />
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
-          <Text style={styles.instructions}>To get started, edit App.js</Text>
-        </View>
-      </>
+      <Router />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#131833',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#fff',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#fff',
-    marginBottom: 5,
-  },
-});

fa4c90bb1f3c92028b908561fa8109228212c68f
DHVSGG <diego@qapla.gg>
2019-06-26 14:37:15 -0500

    Updated function to return only the userName

--- src/services/database.js
+++ src/services/database.js
@@ -3,6 +3,10 @@ import { database } from "../utilities/firebase";
 export const matchesRef = database.ref('/Matches');
 export const usersRef = database.ref('/Users');
 
-export async function getUserDataWithUID(Uid) {
-    return await usersRef.child(Uid).once('value').then((data) => data.val());
+/**
+ * Returns the userName of the specified user
+ * @param {string} Uid User id from firebase
+ */
+export async function getUserNameWithUID(Uid) {
+    return await usersRef.child(Uid).child('userName').once('value').then((data) => data.val());
 }

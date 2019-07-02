64449078a487f9d1e85db1761273c34ebad6631a
DHVSGG <diego@qapla.gg>
2019-07-02 16:32:15 -0500

    AuthLoadingScreen created

---
+++ src/screens/AuthLoadingScreen/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor:'#131833'
+    },
+    textColor: {
+        color: 'rgba(61,249,223,1)',
+        alignSelf: 'center'
+    }
+})

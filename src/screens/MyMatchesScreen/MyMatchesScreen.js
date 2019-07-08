16c93a8af957fd69f53ab7cc08d380a6b9ec5457
DHVSGG <diego@qapla.gg>
2019-07-08 18:12:23 -0500

    ChooseMatchType redirect and possible correction for bug1 implemented

--- src/screens/MyMatchesScreen/MyMatchesScreen.js
+++ src/screens/MyMatchesScreen/MyMatchesScreen.js
@@ -7,41 +7,71 @@ import { connect } from 'react-redux';
 
 export class MyMatchesScreen extends Component {
     state = {
-        matches: [],
+        matches: []
     };
 
-    componentWillReceiveProps(props){}
-
     componentDidMount(){
-        /**
-         * This event is triggered when the user goes to other screen
-         */
-        const didBlurSubscription = this.props.navigation.addListener(
-            'didBlur',
-            (payload) => {
-                /**
-                 * Remove the listeners on matchesRef and clean the state
-                 */
-                matchesRef.off();
-                this.setState({ matches: [] });
-            }
-        );
-        /**
-         * This event is triggered when the user enter (focus) on this screen
-         */
-        const didFocusSubscription = this.props.navigation.addListener(
-            'didFocus',
-            (payload) => {
-                /**
-                 * Listen to childs_added for matches where te adversary1 is the current user (the user is the creator of the match)
-                 */
-                matchesRef.orderByChild('adversary1').equalTo(this.props.id).on('child_added', async (hostedMatches) => {
-                    if (hostedMatches.val().adversary2 !== '') {
+        this.list = [
+            /**
+             * This event is triggered when the user goes to other screen
+             */
+            this.props.navigation.addListener(
+                'didBlur',
+                (payload) => {
+                    /**
+                     * Remove the listeners on matchesRef and clean the state
+                     */
+                    matchesRef.off();
+                    this.setState({ matches: [] });
+                }
+            ),
+            /**
+             * This event is triggered when the user enter (focus) on this screen
+             */
+            this.props.navigation.addListener(
+                'didFocus',
+                (payload) => {
+                    /**
+                     * Listen to childs_added for matches where te adversary1 is the current user (the user is the creator of the match)
+                     */
+                    matchesRef.orderByChild('adversary1').equalTo(this.props.id).on('child_added', async (hostedMatches) => {
+                        if (hostedMatches.val().adversary2 !== '') {
+                            //Take the necesary information from the object returned of the database
+                            const { adversary2, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = hostedMatches.val();
+                            //Object with the necesary fields to load the match in the app (the card and the detailed view)
+                            const matchObject = {
+                                adversary2,
+                                alphaNumericIdMatch,
+                                bet,
+                                date,
+                                game,
+                                hour,
+                                idMatch,
+                                numMatches,
+                                observations,
+                                platform,
+                                timeStamp,
+                                winBet,
+                                //Get the userName from a external function because the match object only have the UID
+                                userName: await getUserNameWithUID(hostedMatches.val().adversary2).then((userName) => userName),
+                                gamerTag: await getGamerTagWithUID(hostedMatches.val().adversary2, hostedMatches.val().game, hostedMatches.val().platform).then((gamerTag) => gamerTag)
+                            };
+                            this.setState((state) => {
+                                //Add the matchObject to the matches array of the state
+                                const matches = [...state.matches, matchObject];
+                                return { matches };
+                            });
+                        }
+                    });
+                    /**
+                     * Listen to childs_added for matches where te adversary2 is the current user (the user has accepted the challenge of other user)
+                     */
+                    matchesRef.orderByChild('adversary2').equalTo(this.props.id).on('child_added', async (challengedMatches) => {
                         //Take the necesary information from the object returned of the database
-                        const { adversary2, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = hostedMatches.val();
+                        const { adversary1, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = challengedMatches.val();
                         //Object with the necesary fields to load the match in the app (the card and the detailed view)
                         const matchObject = {
-                            adversary2,
+                            adversary1,
                             alphaNumericIdMatch,
                             bet,
                             date,
@@ -54,93 +84,68 @@ export class MyMatchesScreen extends Component {
                             timeStamp,
                             winBet,
                             //Get the userName from a external function because the match object only have the UID
-                            userName: await getUserNameWithUID(hostedMatches.val().adversary2).then((userName) => userName),
-                            gamerTag: await getGamerTagWithUID(hostedMatches.val().adversary2, hostedMatches.val().game, hostedMatches.val().platform).then((gamerTag) => gamerTag)
+                            userName: await getUserNameWithUID(challengedMatches.val().adversary1).then((userName) => userName),
+                            gamerTag: await getGamerTagWithUID(challengedMatches.val().adversary1, challengedMatches.val().game, challengedMatches.val().platform).then((gamerTag) => gamerTag)
                         };
                         this.setState((state) => {
                             //Add the matchObject to the matches array of the state
                             const matches = [...state.matches, matchObject];
                             return { matches };
                         });
-                    }
-                });
-                /**
-                 * Listen to childs_added for matches where te adversary2 is the current user (the user has accepted the challenge of other user)
-                 */
-                matchesRef.orderByChild('adversary2').equalTo(this.props.id).on('child_added', async (challengedMatches) => {
-                    //Take the necesary information from the object returned of the database
-                    const { adversary1, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = challengedMatches.val();
-                    //Object with the necesary fields to load the match in the app (the card and the detailed view)
-                    const matchObject = {
-                        adversary1,
-                        alphaNumericIdMatch,
-                        bet,
-                        date,
-                        game,
-                        hour,
-                        idMatch,
-                        numMatches,
-                        observations,
-                        platform,
-                        timeStamp,
-                        winBet,
-                        //Get the userName from a external function because the match object only have the UID
-                        userName: await getUserNameWithUID(challengedMatches.val().adversary1).then((userName) => userName),
-                        gamerTag: await getGamerTagWithUID(challengedMatches.val().adversary1, challengedMatches.val().game, challengedMatches.val().platform).then((gamerTag) => gamerTag)
-                    };
-                    console.log(matchObject);
-                    this.setState((state) => {
-                        //Add the matchObject to the matches array of the state
-                        const matches = [...state.matches, matchObject];
-                        return { matches };
                     });
-                });
-                /**
-                 * Listen to detect when a user accept a challenge from the current user (adversary2 field is filled) or remove if adversary cancels their
-                 * participation on the match
-                 */
-                matchesRef.orderByChild('adversary1').equalTo(this.props.id).on('child_changed', async (updatedMatch) => {
-                    var copyOfMatches = [...this.state.matches];
-                    const indexToUpdate = copyOfMatches.findIndex((element) => element.idMatch === updatedMatch.key);
-                    if (updatedMatch.val().adversary2 !== '') {
-                        if (indexToUpdate !== -1) {
-                            copyOfMatches[indexToUpdate] = updatedMatch.val();
-                            copyOfMatches[indexToUpdate].userName = await getUserNameWithUID(updatedMatch.val().adversary2).then((userName) => userName);
-                            copyOfMatches[indexToUpdate].gamerTag = await getGamerTagWithUID(updatedMatch.val().adversary2, updatedMatch.val().game, updatedMatch.val().platform).then((gamerTag) => gamerTag);
+                    /**
+                     * Listen to detect when a user accept a challenge from the current user (adversary2 field is filled) or remove if adversary cancels their
+                     * participation on the match
+                     */
+                    matchesRef.orderByChild('adversary1').equalTo(this.props.id).on('child_changed', async (updatedMatch) => {
+                        var copyOfMatches = [...this.state.matches];
+                        const indexToUpdate = copyOfMatches.findIndex((element) => element.idMatch === updatedMatch.key);
+                        if (updatedMatch.val().adversary2 !== '') {
+                            if (indexToUpdate !== -1) {
+                                copyOfMatches[indexToUpdate] = updatedMatch.val();
+                                copyOfMatches[indexToUpdate].userName = await getUserNameWithUID(updatedMatch.val().adversary2).then((userName) => userName);
+                                copyOfMatches[indexToUpdate].gamerTag = await getGamerTagWithUID(updatedMatch.val().adversary2, updatedMatch.val().game, updatedMatch.val().platform).then((gamerTag) => gamerTag);
+                            } else {
+                                copyOfMatches.push(updatedMatch.val());
+                                copyOfMatches[copyOfMatches.length - 1].userName = await getUserNameWithUID(updatedMatch.val().adversary2).then((userName) => userName);
+                                copyOfMatches[copyOfMatches.length - 1].gamerTag = await getGamerTagWithUID(updatedMatch.val().adversary2, updatedMatch.val().game, updatedMatch.val().platform).then((gamerTag) => gamerTag);
+                            }
                         } else {
-                            copyOfMatches.push(updatedMatch.val());
-                            copyOfMatches[copyOfMatches.length - 1].userName = await getUserNameWithUID(updatedMatch.val().adversary2).then((userName) => userName);
-                            copyOfMatches[copyOfMatches.length - 1].gamerTag = await getGamerTagWithUID(updatedMatch.val().adversary2, updatedMatch.val().game, updatedMatch.val().platform).then((gamerTag) => gamerTag);
+                            copyOfMatches.splice(indexToUpdate, 1);
                         }
-                    } else {
-                        copyOfMatches.splice(indexToUpdate, 1);
-                    }
-                    this.setState((state) => {
-                        //Add the matchObject to the matches array of the state
-                        const matches = [...copyOfMatches];
-                        return { matches };
+                        this.setState((state) => {
+                            //Add the matchObject to the matches array of the state
+                            const matches = [...copyOfMatches];
+                            return { matches };
+                        });
                     });
-                });
-                /**
-                 * Add a listener of type child_removed.
-                 * When a child is removed from matches the event is triggered to remove the match from the state
-                 * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
-                 */
-                matchesRef.on('child_removed', (removedPublicMatch) => {
-                    //Get the id of the match
-                    const { idMatch } = removedPublicMatch.val();
-                    //Create a new array and assign the filtered original array
-                    //The filter return every match except the match who id is the same that idMatch
-                    const matchesArrayFiltered = this.state.matches.filter((match) =>(idMatch !== match.idMatch));
-                    this.setState((state) => {
-                        //Asiggn the filteredArray to the state
-                        const matches = [...matchesArrayFiltered];
-                        return { matches };
+                    /**
+                     * Add a listener of type child_removed.
+                     * When a child is removed from matches the event is triggered to remove the match from the state
+                     * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
+                     */
+                    matchesRef.on('child_removed', (removedPublicMatch) => {
+                        //Get the id of the match
+                        const { idMatch } = removedPublicMatch.val();
+                        //Create a new array and assign the filtered original array
+                        //The filter return every match except the match who id is the same that idMatch
+                        const matchesArrayFiltered = this.state.matches.filter((match) =>(idMatch !== match.idMatch));
+                        this.setState((state) => {
+                            //Asiggn the filteredArray to the state
+                            const matches = [...matchesArrayFiltered];
+                            return { matches };
+                        });
                     });
-                });
-            }
-        );
+                }
+            )
+        ];
     }
+
+    componentWillUnmount() {
+        //Remove didBlur and didFocus listeners on navigation
+        this.list.forEach((item) => item.remove());
+    }
+
     render() {
         return (
             <View style={styles.container}>
--- src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
+++ src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
@@ -13,87 +13,93 @@ class PublicMatchesFeedScreen extends Component {
     };
 
     componentWillMount(){
-        /**
-         * This event is triggered when the user goes to other screen
-         */
-        const didBlurSubscription = this.props.navigation.addListener(
-            'didBlur',
-            (payload) => {
-                /**
-                 * Remove the listeners on matchesRef and clean the state
-                 */
-                matchesRef.off();
-                this.setState({ matches: [] });
-            }
-        );
-        /**
-         * This event is triggered when the user enter (focus) on this screen
-         */
-        const didFocusSubscription = this.props.navigation.addListener(
-            'didFocus',
-            (payload) => {
-                /**
-                 * Add a listener of type child_added. In the first load bring all the
-                 * childs of the matches node.
-                 * When a new child is added the event is triggered to add the new match
-                 * to the state
-                 * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
-                 */
-                matchesRef.on('child_added', async (newPublicMatch) => {
-                    //Take the necesary information from the object returned of the database
-                    const { adversary1, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = newPublicMatch.val();
-                    //Object with the necesary fields to load the match in the app (the card and the detailed view)
-                    const matchObject = {
-                        adversary1,
-                        alphaNumericIdMatch,
-                        bet,
-                        date,
-                        game,
-                        hour,
-                        idMatch,
-                        numMatches,
-                        observations,
-                        platform,
-                        timeStamp,
-                        winBet,
-                        //Get the userName from a external function because the match object only have the UID
-                        userName: await getUserNameWithUID(newPublicMatch.val().adversary1).then((userName) => userName),
-                        gamerTag: await getGamerTagWithUID(newPublicMatch.val().adversary1, newPublicMatch.val().game, newPublicMatch.val().platform).then((gamerTag) => gamerTag)
-                    };
-                    this.setState((state) => {
-                        //Add the matchObject to the matches array of the state
-                        const matches = [...state.matches, matchObject];
-                        return { matches };
+        this.list = [
+            /**
+             * This event is triggered when the user goes to other screen
+             */
+            this.props.navigation.addListener(
+                'didBlur',
+                (payload) => {
+                    /**
+                     * Remove the listeners on matchesRef and clean the state
+                     */
+                    matchesRef.off();
+                    this.setState({ matches: [] });
+                }
+            ),
+            /**
+             * This event is triggered when the user enter (focus) on this screen
+             */
+            this.props.navigation.addListener(
+                'didFocus',
+                (payload) => {
+                    /**
+                     * Add a listener of type child_added. In the first load bring all the
+                     * childs of the matches node.
+                     * When a new child is added the event is triggered to add the new match
+                     * to the state
+                     * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
+                     */
+                    matchesRef.on('child_added', async (newPublicMatch) => {
+                        //Take the necesary information from the object returned of the database
+                        const { adversary1, alphaNumericIdMatch, bet, date, game, hour, idMatch, numMatches, observations, platform, timeStamp, winBet } = newPublicMatch.val();
+                        //Object with the necesary fields to load the match in the app (the card and the detailed view)
+                        const matchObject = {
+                            adversary1,
+                            alphaNumericIdMatch,
+                            bet,
+                            date,
+                            game,
+                            hour,
+                            idMatch,
+                            numMatches,
+                            observations,
+                            platform,
+                            timeStamp,
+                            winBet,
+                            //Get the userName from a external function because the match object only have the UID
+                            userName: await getUserNameWithUID(newPublicMatch.val().adversary1).then((userName) => userName),
+                            gamerTag: await getGamerTagWithUID(newPublicMatch.val().adversary1, newPublicMatch.val().game, newPublicMatch.val().platform).then((gamerTag) => gamerTag)
+                        };
+                        this.setState((state) => {
+                            //Add the matchObject to the matches array of the state
+                            const matches = [...state.matches, matchObject];
+                            return { matches };
+                        });
                     });
-                });
-                /**
-                 * Add a listener of type child_removed.
-                 * When a child is removed from matches the event is triggered to remove the match
-                 * from the state
-                 * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
-                 */
-                matchesRef.on('child_removed', (removedPublicMatch) => {
-                    //Get the id of the match
-                    const { idMatch } = removedPublicMatch.val();
-                    //Create a new array and assign the filtered original array
-                    //The filter return every match except the match who id is the same that idMatch
-                    const matchesArrayFiltered = this.state.matches.filter((match) => (idMatch !== match.idMatch));
-                    this.setState((state) => {
-                        //Asiggn the filteredArray to the state
-                        const matches = [...matchesArrayFiltered];
-                        return { matches };
+                    /**
+                     * Add a listener of type child_removed.
+                     * When a child is removed from matches the event is triggered to remove the match
+                     * from the state
+                     * Reference: https://firebase.google.com/docs/database/web/lists-of-data?hl=es-419
+                     */
+                    matchesRef.on('child_removed', (removedPublicMatch) => {
+                        //Get the id of the match
+                        const { idMatch } = removedPublicMatch.val();
+                        //Create a new array and assign the filtered original array
+                        //The filter return every match except the match who id is the same that idMatch
+                        const matchesArrayFiltered = this.state.matches.filter((match) => (idMatch !== match.idMatch));
+                        this.setState((state) => {
+                            //Asiggn the filteredArray to the state
+                            const matches = [...matchesArrayFiltered];
+                            return { matches };
+                        });
                     });
-                });
-            }
-        );
+                }
+            ) // End of didFocus listener
+        ];
+    }
+
+    componentWillUnmount() {
+        //Remove didBlur and didFocus listeners on navigation
+        this.list.forEach((item) => item.remove());
     }
 
     onCrearRetaButtonPress() {
         if(!this.props.navigation.getParam('firstMatchCreated')){
             storeData('first-match-created', 'true');
         }
-
-        this.props.navigation.navigate(isUserLogged() ? 'Publicas' : 'SignIn');
+        this.props.navigation.navigate(isUserLogged() ? 'ChooseMatchType' : 'SignIn');
     }
 
     render() {

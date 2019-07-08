4aee0ede56d48e0a835307e8669fff6a46c22ef7
DHVSGG <diego@qapla.gg>
2019-07-08 18:10:30 -0500

    ChooseMatchTypeScreen created

---
+++ src/screens/ChooseMatchTypeScreen/ChooseMatchTypeScreen.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { Svg } from 'react-native-svg';
+import styles from './style';
+import Images from '../../../assets/images';
+
+const LightningIcon = Images.svg.lightningIcon;
+const SearchIcon = Images.svg.searchIcon;
+
+class ChooseMatchTypeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Escoge un tipo de reta</Text>
+                <View style={styles.lightningImage}>
+                    <Svg>
+                        <LightningIcon />
+                    </Svg>
+                </View>
+                <TouchableWithoutFeedback>
+                    <View style={styles.publicMatchButton}>
+                        <Text style={styles.publicMatchButtonText}>
+                            RETA PÚBLICA
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.directMatchButton}>
+                        <View style={styles.directMatchButtonSearchIcon}>
+                            <Svg>
+                                <SearchIcon width={18} height={18} />
+                            </Svg>
+                        </View>
+                        <Text style={styles.publicMatchButtonText}>
+                            RETAR USUARIO
+                        </Text>
+                        <View style={styles.directMatchButtonSearchIcon}></View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
+        );
+    }
+}
+
+export default ChooseMatchTypeScreen;
---
+++ src/screens/ChooseMatchTypeScreen/style.js
@@ -0,0 +1,69 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:'#131833'
+    },
+    titleText: {
+        color: '#FFF',
+        marginLeft: 24,
+        fontSize: 32,
+        fontWeight: 'bold',
+        width: '60%',
+        marginTop: 43
+    },
+    lightningImage: {
+        marginTop: 71,
+        alignItems: 'center'
+    },
+    publicMatchButton: {
+        marginTop: 66,
+        borderRadius: 100,
+        backgroundColor: '#FA2D79',
+        elevation: 6,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+        marginRight: 80,
+        marginLeft: 80
+    },
+    publicMatchButtonText: {
+        color: '#FFF',
+        fontSize: 14,
+        letterSpacing: .5,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        textAlign: 'center'
+    },
+    directMatchButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:'flex-start',
+        borderRadius: 100,
+        borderWidth: 3,
+        borderStyle: 'solid',
+        borderColor: '#6D7DDE',
+        backgroundColor: 'transparent',
+        marginTop: 25,
+        marginRight: 80,
+        marginLeft: 80
+    },
+    directMatchButtonSearchIcon: {
+        marginTop: 14,
+        marginBottom: 14,
+        marginLeft: 15
+    },
+    directMatchButtonText: {
+        color: 'rgba(0, 0, 0, .8)',
+        fontSize: 14,
+        letterSpacing: .5,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        alignContent: 'center'
+    }
+});

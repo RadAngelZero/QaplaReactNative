4db728f0ad3c05ff33b7473103a8d07228f20ae1
DHVSGG <diego@qapla.gg>
2019-07-10 11:57:52 -0500

    Remove unnecesary (and duplicated) code

--- android/app/src/main/java/com/qaplareactnative/MainApplication.java
+++ android/app/src/main/java/com/qaplareactnative/MainApplication.java
@@ -34,7 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNGoogleSigninPackage(),
-            new FBSDKPackage(),
             new FBSDKPackage(mCallbackManager),
             new SvgPackage(),
             new RNGestureHandlerPackage()

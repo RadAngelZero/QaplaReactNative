6930535f24e060a59317f63a605c93b6ba1a86d3
DHVSGG <diego@qapla.gg>
2019-07-16 16:07:08 -0500

    Modal component created

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import styles from './style';
+
+class Modal extends Component {
+    render() {
+        return (
+            <>
+                {this.props.open &&
+                    <>
+                        <View style={styles.overlay} />
+                        <View style={styles.mainContainer}>
+                            <View style={styles.container}>
+                                <View style={styles.modalBody}>
+                                    <Text style={styles.closeIcon} onPress={this.props.onClose}>X</Text>
+                                    {this.props.children}
+                                </View>
+                            </View>
+                        </View>
+                    </>
+                }
+            </>
+        );
+    }
+}
+
+export default Modal;

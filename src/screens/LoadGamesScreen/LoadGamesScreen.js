ccfe0d0489561bb33a9ff5dea7aa814c5caed5fb
DHVSGG <diego@qapla.gg>
2019-07-16 16:12:43 -0500

    Navigation update on GameCard and LoadGames

--- src/screens/LoadGamesScreen/LoadGamesScreen.js
+++ src/screens/LoadGamesScreen/LoadGamesScreen.js
@@ -1,4 +1,5 @@
 // josep.sanahuja - 15-07-2019 - us25 - + addGameProfile Modal logic
+// diego          - 16-07-2019 - us30 - update navigation when GamerTag is added
 
 import React from 'react';
 
@@ -169,7 +170,7 @@ class LoadGamesScreen extends React.Component {
                                         this.props.loadUserData(this.props.user.id);
                                         
                                         // Navigate to the screen where Qaploins are selected
-                                        navigate('SetBet', {game: this.props.selectedGame});
+                                        navigate('SetBet', {game: {gameKey: this.props.selectedGame.gameKey, platform: this.props.selectedGame.platform}});
                                         console.log("[render]: - hasNoGameOnProfile");
                                     }
                                     else{

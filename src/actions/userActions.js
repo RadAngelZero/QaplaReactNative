6fdcf64147d2bdc7abaeea0fbc33725e1c7ea792
DHVSGG <diego@qapla.gg>
2019-08-01 14:30:18 -0500

    User redux modified

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,15 +1,29 @@
-import { GET_USER_DATA } from '../utilities/Constants';
+// diego -          01-08-2019 - us58 - Change the way to load the user data and the way for listen changes
+import { UPDATE_USER_DATA, REMOVE_USER_DATA } from '../utilities/Constants';
 import { usersRef } from '../services/database';
 
 export const getUserNode = (uid) => (dispatch) => {
-    usersRef.child(uid).on('value', (user) => {
-        dispatch(getUserDataSuccess(user.val()));
+    usersRef.child(uid).on('child_added', (childAdded) => {
+        dispatch(updateUserDataSuccess({ key: childAdded.key, value: childAdded.val() }));
+    });
+    usersRef.child(uid).on('child_changed', (childChanged) => {
+        dispatch(updateUserDataSuccess({ key: childChanged.key, value: childChanged.val() }));
+    });
+    usersRef.child(uid).on('child_removed', (childRemoved) => {
+        dispatch(removeUserData({ key: childRemoved.key, value: childRemoved.val() }));
     });
 }
 
-export const getUserDataSuccess = (payload) => {
+export const updateUserDataSuccess = (payload) => {
     return {
-        type: GET_USER_DATA,
+        type: UPDATE_USER_DATA,
         payload
-    };
+    }
+}
+
+export const removeUserData = (payload) => {
+    return {
+        type: REMOVE_USER_DATA,
+        payload
+    }
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,19 @@
-import { GET_USER_DATA } from '../utilities/Constants';
+// diego -          01-08-2019 - us58 - Change the way to load the user data and the way for listen changes
+import { UPDATE_USER_DATA, REMOVE_USER_DATA } from '../utilities/Constants';
 
 const initialState = {
     user: {}
 };
 
 function userReducer(state = initialState, action) {
+      const user = {...state.user};
       switch (action.type) {
-        case GET_USER_DATA:
-            return { ...state, user: action.payload };
+        case UPDATE_USER_DATA:
+            user[action.payload.key] = action.payload.value;
+            return { ...state, user };
+        case REMOVE_USER_DATA:
+            delete user[action.payload.key];
+            return { ...state, user };
         default:
             return state;
       }

009ba1adfa5d9dc25ef78d6c3da8592358add682
Josep Maria <jtomas.sanahuja@gmail.com>
2019-08-02 13:34:03 -0500

    +Not show up logic & +Style & +nav logic adjustment

--- src/screens/SetBetScreen/SetBetScreen.js
+++ src/screens/SetBetScreen/SetBetScreen.js
@@ -1,7 +1,8 @@
-// diego -          24-07-2019 - us31 - updated createMatch and decreaseBet to accept
+// josep.sanahuja - 01-08-2019 - us57 - + Modal for 10 minutes msg when creating a match
+// diego          - 24-07-2019 - us31 - Updated createMatch and decreaseBet to accept
 //                                      bets from 0 qaploins
-// diego -          16-07-2019 - us 34 - Substract of qaploins logic implemented
-// diego -          15-07-2019 - us 27 - added increment bet option
+// diego          - 16-07-2019 - us34 - Substract of qaploins logic implemented
+// diego          - 15-07-2019 - us27 - added increment bet option
 
 import React, { Component } from 'react';
 
@@ -10,10 +11,13 @@ import {
     View,
     Text,
     BackHandler,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    Switch
 } from 'react-native';
 
 import styles from './style';
+
+import Modal from '../../components/Modal/Modal'; 
 import { connect } from 'react-redux';
 import Svg from 'react-native-svg';
 import images from './../../../assets/images';
@@ -45,12 +49,18 @@ class SetBetScreen extends Component {
             currentBet: 150,
             open: false,
             loading: false,
-            timeActionMsgOpen: false
+            timeActionMsgOpen: false,
+            swtichTA: false
         };
     }
 
     async componentWillMount() {
-        const com = await getCurrentQaplaCommission()
+        try {
+            const com = await getCurrentQaplaCommission();    
+        } catch(err) {
+            console.log(err);
+        }
+
         this.setState({ commission: com });
     }
 
@@ -85,14 +95,24 @@ class SetBetScreen extends Component {
 
     async createMatch() {
        if (!this.state.loading && this.props.userQaploins >= this.state.currentBet) {
+            // this.state.loading is used as a mechanism to prevent users to press the 
+            // Android back button and create several matches at the same time
             this.setState({ loading: true });
+
             try {
                 await createPublicMatch(this.props.uid, this.state.currentBet, this.props.selectedGame);
                 await substractQaploinsToUser(this.props.uid, this.props.userQaploins, this.state.currentBet);
                 
-                let openMsgFlag = await JSON.parse(retrieveData('create-match-time-action-msg'));
+                // When retrieving the flag from AsyncStorage if it hasn't been stored yet, it will
+                // return a 'null' value, otherwise it would return a 'false' 'true' value from a
+                // previous flag update.
+                let openMsgFlag = JSON.parse(await retrieveData('create-match-time-action-msg'));
 
-                if (!this.state.timeActionMsgOpen && openMsgFlag) {
+                // When creating a match 'this.state.timeActionMsgOpen' is expected to be false, 
+                // otherwise when loading the Component the Modal would automatically open, which is
+                // a behaviour we don't want. 
+                if (!this.state.timeActionMsgOpen && (openMsgFlag || openMsgFlag === null)) {
+                    
                     // Toogle modal state to open
                     this.setState({
                         timeActionMsgOpen: !this.state.timeActionMsgOpen
@@ -109,28 +129,50 @@ class SetBetScreen extends Component {
         }
     }
 
+    /**
+     * Description:
+     * Toogles switchTA value
+     */
+    toggleSwitchTA() {
+        this.setState({
+            switchTA: !this.state.switchTA
+        });
+    }
+
     render() {
         return (
             <SafeAreaView style={styles.container}>
                 <BuyQaploinsModal open={this.state.open} onClose={() => this.setState({ open: false })} />
-                <Modal open={this.state.timeActionMsgOpen} onClose={() => this.setState({ timeActionMsgOpen: false })}>
-                    <View style={styles.container}>
-                        <QaploinIcon height={40} width={40} />
-                        <Text style={styles.qaploinsToBuyText}>750</Text>
-                        <Text style={styles.qaploinsText}>Qaploins</Text>
+                <Modal open={this.state.timeActionMsgOpen} onClose={() => {
+                    this.setState({
+                        timeActionMsgOpen: false 
+                    });
+                    this.setState({
+                        loading: false })
+                    }}>
+                    <View style={styles.containerMsgModal}>
+                        <Text style={styles.headerText}>Recuerda</Text>
                         <Text style={styles.paragraph}>
-                            Puedes devolver los 750 Qaploins cuando quieras ¡y te haremos un reembolso!
+                            La Reta expirará después de 10 minutos si no recibe y se acepta un desafio de otro jugador.                  
                         </Text>
+                        <Text style={[styles.smallText, {marginBottom: '2%', marginTop: '20%'}]}>No volver a mostrar este mensaje</Text>
+                        <Switch
+                            style = {styles.switch}
+                            trackColor={{true: '#36E5CE', false: 'grey'}}
+                            onValueChange = {this.toggleSwitchTA.bind(this)}
+                            value = {this.state.switchTA} /> 
                         <TouchableWithoutFeedback onPress={() => {
-                            // Store flag that indicate that the msg have to be shown
-                            storeData('create-match-time-action-msg');
-                            // Navigate to the Public Match Feed screen
+                            // If switch is activated then that means that this screen should
+                            // not show up again when creating a Match. This is why we set flag value
+                            // to false, indicating that the modal should not open again.
+                            if (this.state.switchTA) {
+                                storeData('create-match-time-action-msg', JSON.stringify(false));
+                            }
                             this.props.navigation.navigate('Publicas')}}>
-                            <View style={styles.buyButton}>
-                                <Text style={styles.priceText}>$50 MXN</Text>
+                            <View style={styles.okButton}>
+                                <Text style={styles.buttonText}>Confirmar</Text>
                             </View>
                         </TouchableWithoutFeedback>
-                        <Text style={styles.smallText}>Costo por devolución $10 MXN</Text>
                     </View>
                 </Modal>
                 <View style={styles.headerOptions}>

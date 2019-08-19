e3d131105281ed5ed7dc16893f7463be151b0624
DHVSGG <diego@qapla.gg>
2019-08-19 18:00:54 -0500

    Add navigation to User profile on router

---
+++ src/screens/UserProfileScreen/UserProfileScreen.js
@@ -0,0 +1,14 @@
+// diego           - 19-08-2019 - us89 - File creation
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export class UserProfileScreen extends Component {
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
+
+export default UserProfileScreen;

9c78f891776b6d86f237dfd629b6dc56a0fa09c7
DHVSGG <diego@qapla.gg>
2019-08-20 22:12:12 -0500

    Load user games statistics on redux

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,17 +1,17 @@
-// diego -          01-08-2019 - us58 - Change the way to load the user data and the way for listen changes
+// diego           - 20-08-2019 - us89 - Load user games statistics
+// diego           - 01-08-2019 - us58 - Change the way to load the user data and the way for listen changes
 
-import {
-    UPDATE_USER_DATA,
-    REMOVE_USER_DATA
-} from '../utilities/Constants';
-import { usersRef } from '../services/database';
+import { UPDATE_USER_DATA, REMOVE_USER_DATA } from '../utilities/Constants';
+import { usersRef, gamesRef, gamersRef } from '../services/database';
 
-export const getUserNode = (uid) => (dispatch) => {
+export const getUserNode = (uid) => async (dispatch) => {
+
     /*
         us58: The way to load the user data is now by listening the different events in the
         database
     */
     usersRef.child(uid).on('child_added', (childAdded) => {
+
         /*
             This event is triggered for two reasons, first when getUserNode is called
             and other when a new child is added to the users node, like the notifications
@@ -21,6 +21,7 @@ export const getUserNode = (uid) => (dispatch) => {
     });
 
     usersRef.child(uid).on('child_changed', (childChanged) => {
+
         /*
             This event is triggered when a child of the user node already exists but is updated
             like the number of qaploins (credits child), always exists, but their value can change
@@ -30,12 +31,27 @@ export const getUserNode = (uid) => (dispatch) => {
     });
 
     usersRef.child(uid).on('child_removed', (childRemoved) => {
+
         /*
             This event is triggered when a child of the user node is removed (deleted), as the
             child_added we can  think in the notifications or notificationMatch node, if the user
             have no notifications this nodes are removed, and we need to detect that
         */
         dispatch(removeUserData({ key: childRemoved.key, value: childRemoved.val() }));
+    });
+
+    // Pass everything to async to can perform actions sequentially
+    const platformsWithGames = await gamesRef.once('value');
+    platformsWithGames.forEach((platformGames) => {
+        Object.keys(platformGames.val()).forEach((gameKey) => {
+            gamersRef.child(gameKey).orderByChild('userUid').equalTo(uid).on('value', (gamerGameData) => {
+                if (gamerGameData.exists()) {
+                    gamerGameData.forEach((gamerProfile) => {
+                        dispatch(updateUserDataSuccess({ key: gameKey, value: gamerProfile.val() }));
+                    });
+                }
+            });
+        });
     });
 }
 

063a2f59c3d9cfa6bc10b54fbc3cbc4737adf89c
Josep Maria <jtomas.sanahuja@gmail.com>
2019-08-27 15:42:45 -0500

    Update in HighlightModal

--- src/components/HighlightModal/HighlightModal.js
+++ src/components/HighlightModal/HighlightModal.js
@@ -19,6 +19,8 @@ const TOP_QUADRANT_BOTTOM     = 3;
 const BOTTOM_QUADRANT_TOP     = 4;
 const BOTTOM_QUADRANT_BOTTOM  = 5;
 
+
+
 class HighlightModal extends Component {
   
   constructor(props) {
@@ -30,22 +32,27 @@ class HighlightModal extends Component {
       elemPosX: 0,
       elemPosY: 0,
       elemHeight: 0,
-      elemWidth: 0
+      elemWidth: 0,
+      flag: false,
+      flag2: false
     };
+
+    this.quad = null;
+    this.subQuad = null;
   }
 
-  /**
+   /**
    *  Callbacks used in render()
    */
 
-  /**
-   * Description:
-   * Perform a series of actions for the Modal, including the main one which is 'closing the modal'
-   * and navigation to next screen. The other actions are performed via cb1 and cb2 props,
-   * which are executed sequentially, cb1 1st, cb2 2nd, and they are executed in a synchronous way.
-   * 
-   * @param None
-   */
+   /**
+    * Description:
+    * Perform a series of actions for the Modal, including the main one which is 'closing the modal'
+    * and navigation to next screen. The other actions are performed via cb1 and cb2 props,
+    * which are executed sequentially, cb1 1st, cb2 2nd, and they are executed in a synchronous way.
+    * 
+    * @param None
+    */
     action = async () => {
       // Close the Modal
       this.closeModal();
@@ -66,37 +73,63 @@ class HighlightModal extends Component {
         this.props.navigation.navigate(this.props.nextScreen);
       }
     }
-      
+
     /**
-   * Description:
-   * Closes the Modal by using the function given in props.
-   * 
-   * @param None
-   */
+    * Description:
+    * Closes the Modal by using the function given in props.
+    * 
+    * @param None
+    */
     closeModal = async () => {
       // Close the Modal
       this.props.onClose();
     }
 
     measureChildComponent = (event) => {
-      setTimeout( () => {
-      this.childWrapper.current.measureInWindow((pageX, pageY, width, height) => {
-        console.log('* Component width is: ' + width)
-        console.log('* Component height is: ' + height)
-        console.log('* X offset to page: ' + pageX)
-        console.log('*Y offset to page: ' + pageY)
+      
+      if (!this.state.timeoutStarted) {
+        
+        // 27-08-2019: measureInWindow can get invalid and non intended values for pageX, pageY,
+        // width, height. Therefore, a setTimeOut what makes is to delay the calculus
+        // that gets layout info for our object in the final position on screen in absolute
+        // values. It can depend on whether the element is rendered after a scrollview that 
+        // nurtures from data and therefore it delays the correct position of our object because
+        // it does not expand the view completely. (At least thats a theory, you can play with
+        // that and add more details here in the documentation)
+        setTimeout( () => {
 
-        if ((this.state.elemPosX !== pageX) || (this.state.elemPosY !== pageY)){
-          
-          this.setState({
-            elemPosX: pageX,
-            elemPosY: pageY,
-            elemHeight: height,
-            elemWidth: width
-          });
-        }
+          // pageX, pageY, width, height are absolute values within the display
+          this.childWrapper.current.measureInWindow((pageX, pageY, width, height) => {
+            console.log('* Component width is: ' + width)
+            console.log('* Component height is: ' + height)
+            console.log('* X offset to page: ' + pageX)
+            console.log('*Y offset to page: ' + pageY)
 
-      }); }, 600);    
+            // Initially elemPosX and elemPosY have 0 value, therefore we prevent and update of
+            // the state, to prevent a re-render. However, at this point this might have 0 consecuences
+            // as a result of the timeoutStarted timeoutEnded flags. Leaving it though.
+            // TODO: Check if the if condition really makes sense to be here.
+            if ((this.state.elemPosX !== pageX) || (this.state.elemPosY !== pageY)){ 
+              this.setState({
+                elemPosX: pageX,
+                elemPosY: pageY,
+                elemHeight: height,
+                elemWidth: width
+              });
+
+              this.setState({
+                timeoutEnded: true
+              });
+            }
+          }); 
+        }, this.props.showDelay);   
+
+        // timeoutStarted indicates that a setTimeout was scheduled for this.props.showDelay. 
+        // timeoutEnded will indicate when the computing of the object layout dimensions is finished. 
+        this.setState({
+              timeoutStarted: true
+        });
+      }  
     }
 
     render() {
@@ -111,8 +144,27 @@ class HighlightModal extends Component {
             })
         );
 
+        // Get Mobile window dimensions (height, width)
         const displayHeight = Dimensions.get('window').height;
         const displayMid = displayHeight / 2;
+        
+        // measures Object is initialized with init values (all 0's), it is updated once
+        // elemPosX / elemPosY / elemWidth / elemHeight changes value then measures has
+        // new internal value.
+        const measures = {
+          elemPosX: elemPosX,
+          elemPosY: elemPosY,
+          elemWidth: elemWidth,
+          elemHeight: elemHeight
+        }
+
+        // If setTimeOut was scheduled and executed then Quadrant and SubQuadrant
+        // can be computed. We save resources by computing them only once since the screen
+        // is in portrait mode always.
+        if (this.state.timeoutEnded && this.quad === null && this.subQuad === null) {
+          this.quad = computeQuadrant(measures);
+          this.subQuad = computeSubQuadrant(measures);
+        }
 
         return (
           <View>
@@ -125,31 +177,16 @@ class HighlightModal extends Component {
             <Modal
               animationType="none"
               transparent={true}
-              visible={visible}
+              visible={(visible && this.state.timeoutEnded)}
               onRequestClose={this.action}>
 
-              <View style={getHighlightContainerStyle({
-                elemPosX: elemPosX,
-                elemPosY: elemPosY,
-                elemWidth: elemWidth,
-                elemHeight: elemHeight
-              })}>
-                <View style={getAbsComponentPosOffsetsStyle({
-                  elemPosX: elemPosX,
-                  elemPosY: elemPosY,
-                  elemWidth: elemWidth,
-                  elemHeight: elemHeight
-                })}>
+              <View style={getHighlightContainerStyle2(measures, quad, subQuad)}>
+                <View style={getAbsComponentPosOffsetsStyle2(measures, quad, subQuad)}>
                   <ChildComponentWithRef style={this.props.children.props.style}/>
                 </View>
 
                 {/* This is the info content from the Component */}
-                <View style={getAbsTextInfoPosOffsetsStyle({
-                  elemPosX: elemPosX,
-                  elemPosY: elemPosY,
-                  elemWidth: elemWidth,
-                  elemHeight: elemHeight
-                })}>
+                <View style={getAbsTextInfoPosOffsetsStyle2(measures, quad, subQuad)}>
                   <Text style={{color:'white', fontSize:30, marginLeft:40, marginRight: 40}}>{this.props.header}</Text>
                   <Text style={{color:'white', fontSize:14, marginLeft:40, marginRight: 40}}>{this.props.body}</Text>
                   <View style={{flexDirection:'row', justifyContent: 'flex-end', marginTop: 10, marginRight: 40}}>
@@ -192,12 +229,51 @@ function getHighlightContainerStyle(measures) {
   return res;
 }
 
+function getHighlightContainerStyle2(measures, quad, subQuad) {
+  let res = null;
+
+  console.log("Quadrant: " + quad);
+
+  if (quad === TOP_QUADRANT){
+    res = styles.highlightTopContainer;
+  }
+  else if (quad === BOTTOM_QUADRANT){
+    res = styles.highlightBottomContainer;
+  }
+  else {
+    console.log("moo");
+  }
+
+  return res;
+}
+
 function getAbsComponentPosOffsetsStyle(measures) {
   const displayHeight = Dimensions.get('window').height;
   const displayMid = displayHeight / 2;
   
   let res = null;
   let quad = computeQuadrant(measures);
+
+  if (quad === TOP_QUADRANT){
+    res = {top: measures.elemPosY, left: measures.elemPosX};
+  }
+  else if (quad === BOTTOM_QUADRANT){
+    
+    // description
+    res = {top: measures.elemPosY - displayMid, left: measures.elemPosX};
+  }
+  else {
+    console.log("moo");
+  }
+  
+  return res;
+}
+
+function getAbsComponentPosOffsetsStyle2(measures, quad, subQuad) {
+  const displayHeight = Dimensions.get('window').height;
+  const displayMid = displayHeight / 2;
+  
+  let res = null;
 
   if (quad === TOP_QUADRANT){
     res = {top: measures.elemPosY, left: measures.elemPosX};
@@ -244,6 +320,35 @@ function getAbsTextInfoPosOffsetsStyle(measures) {
   return res;
 }
 
+function getAbsTextInfoPosOffsetsStyle2(measures, quad, subQuad) {
+  const displayHeight = Dimensions.get('window').height;
+  const displayMid = displayHeight / 2;
+  
+  let res = null;
+
+  if (subQuad === TOP_QUADRANT_TOP){
+    res = {position: 'absolute', top: displayMid - 190};
+  }
+  else if (subQuad === TOP_QUADRANT_BOTTOM){
+    
+    // description
+    res = {position: 'absolute', top: 50};
+  }
+  else if (subQuad === BOTTOM_QUADRANT_TOP){
+    res = {position: 'absolute', bottom: 50};
+  }
+  else if (subQuad === BOTTOM_QUADRANT_BOTTOM){
+    console.log("marramiauuuuu");
+    // description
+    res = {position: 'absolute', bottom: 240};
+  }
+  else {
+    console.log("moo");
+  }
+  
+  return res;
+}
+
 function computeQuadrant(measures) {
   const displayWidth = Dimensions.get('window').width;
   const displayHeight = Dimensions.get('window').height;
@@ -276,18 +381,6 @@ function computeSubQuadrant(measures) {
   let quad = computeQuadrant(measures);
 
   console.log('Quadrant: ' + quad);
-
-  console.log(
-    "displayWidth: "
-    + displayWidth
-    + " displayHeight: "
-    + displayHeight
-    + " displayMid: "
-    + displayMid
-    + "elemPosY: "
-    + measures.elemPosY
-    + "elemPosX: "
-    + measures.elemPosX);
 
   if (quad === TOP_QUADRANT) {
     
@@ -327,6 +420,10 @@ function computeSubQuadrant(measures) {
 
   console.log("SubQuadrant: " + res);
   return res;
+}
+
+HighlightModal.defaultProps = {
+  showDelay: 4000
 }
 
 export default withNavigation(HighlightModal);
--- src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
+++ src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
@@ -213,6 +213,7 @@ class PublicMatchesFeedScreen extends Component {
                 <HighlightModal 
                     visible={this.state.showHg1Modal}
                     onClose={this.toggleHg1Modal}
+                    showDelay={0}
                     cb1={this.markHg1}
                     header='Crea una Reta'
                     body='Empieza a competir con otros jugadores. Crea tu reta y gana!'>

5d1fba51e3f050a7f0a4b5c1a23ff041c490c948
DHVSGG <diego@qapla.gg>
2019-09-03 21:33:08 -0500

    Update OnBoarding according to inVision Mock

--- src/components/CarouselPng/CarouselPng.js
+++ src/components/CarouselPng/CarouselPng.js
@@ -1,120 +1,43 @@
+// diego	  	  - 03-09-2019 - us92 - Update carousel according to inVision design
+
 import React, { Component } from 'react';
-import { View, ScrollView, FlatList, Image, Button, Text, Switch} from 'react-native';
+import { View, ScrollView, Image, Text } from 'react-native';
+import Images from './../../../assets/images';
 
 import { styles } from './style';
 
-import {getDimensions} from '@utilities/iosAndroidDim'
-
-pageIndx = -1;
-
 class CarouselPng extends Component { 
-    
-    render() {
-
-        // Images used by the Carousel
-        const { images} = this.props;
-
-        console.log("[Carousel] : images: " + JSON.stringify(images) + " pageIndex: " + JSON.stringify(pageIndx));
-
-        // If there ara no images to show then the Carousel is not rendered.
-        // NOTE: It can be argued that it is better to render a placeholder image
-        // in case there are no images to show, but that is a UX decision to be made
-        if (images && images.length) {
-            console.log("images.length: " + images.length);
-            return (
-              <FlatList
-                    horizontal
-                    pagingEnabled
-                    initialNumToRender={1}
-                    onMomentumScrollEnd={this.onScrollEnd.bind(this)}
-                    showsHorizontalScrollIndicator={false}
-                    data = {images}
-                    keyExtractor={(item, index) => item.toString()}
-                    renderItem = {({item,index}) => {
-                        return (
-                           <View style={styles.flatListContainer}> 
-                                <Image  style={styles.image} source={item}/>                                        
-                           </View>
-                        );
-                    }}
-                />
-               
-            );
-        }
-
-        console.log("[Carousel] : Carousel has no images.");
-        return null;
-    }
-
-    onScrollEnd(e) {
-        const {emmiter} = this.props;
-
+    onScrollEnd = (e) => {
         let contentOffset = e.nativeEvent.contentOffset;
         let viewSize = e.nativeEvent.layoutMeasurement;
 
         // Divide the horizontal offset by the width of the view to see which page is visible
         let pageNum = Math.floor(contentOffset.x / viewSize.width);
-        console.log('scrolled to page ', pageNum);
 
-        emmiter.notify({pageIndex: pageNum});
+        this.props.setCurrentIndex(pageNum);
     }
 
-
+    render() {
+        return (
+            <ScrollView
+                horizontal
+                pagingEnabled
+                disableIntervalMomentum
+                onScroll={this.onScrollEnd}
+                showsHorizontalScrollIndicator={false}>
+                {this.props.carrouselData.map((slide, index) => (
+                    <View key={`OnBoardingImages${index}`} style={styles.flatListContainer}>
+                        <View style={styles.imageContainer}>
+                            <Image style={styles.image} source={slide.Image}/>
+                        </View>
+                        <Text style={styles.title}>{slide.title}</Text>
+                        <Image style={styles.divider} source={Images.png.divider.img} />
+                        <Text style={styles.description}>{slide.description}</Text>
+                    </View>
+                ))}
+            </ScrollView>
+        );
+    }
 }
 
 export default CarouselPng;
-
-// <ScrollView
-//                         horizontal
-//                         pagingEnabled
-//                         showsHorizontalScrollIndicator={false}
-                        
-//                     >
-//                         {
-//                             images.map((image, index) => (
-//                                 <View key = {index} style={styles.carouselContainer}>
-//                                     <Image style={styles.image} source={image}/>
-//                                     <Text style={styles.text}>{index + 1} / {images.length}</Text>
-//                                       <View style={styles.switchContainer}>
-//                                       <Text style={styles.hideTutorial}>No volver a mostrar Tutorial:</Text>
-//                                       <Switch
-//                                          style = {styles.switch}
-//                                          value = {true}
-//                                       />
-//                                       </View>
-//                                       <Button
-//                                           title="Empieza a Retar"
-//                                           disabled={(index + 1 != images.length) ? true : false}
-//                                           color="#36E5CE"
-//                                           accessibilityLabel="Learn more about this purple button"
-//                                        />
-//                                 </View>
-//                             ))
-//                         }
-//                     </ScrollView>
-
-
-
-
-
-
- // <FlatList
-                    
- //                    horizontal
- //                    pagingEnabled
- //                    showsHorizontalScrollIndicator={false}
- //                    data = {images}
- //                    keyExtractor={(item, index) => item.toString()}
- //                    renderItem = {(item, index) => {
- //                        // this.pageIndexUpdate(index); 
- //                        // console.log("Miau " + JSON.stringify(item));
-                         
- //                        return (
- //                            <View style={styles.flatListContainer}> 
- //                                <Image style={styles.image} source={item}/>                                        
- //                            </View>
- //                        );
- //                    }
-
- //                }
- //                />
--- src/screens/WelcomeOnboardingScreen/style.js
+++ src/screens/WelcomeOnboardingScreen/style.js
@@ -1,53 +1,42 @@
+// diego	  	     - 03-09-2019 - us92 - Update Welcome styles according to inVision design
 // josep.sanahuja    - 05-08-2019 - us84 - + sfvContainer
 
 import {StyleSheet} from 'react-native'
-
-import {getDimensions} from '@utilities/iosAndroidDim'
+import { getDimensions } from '../../utilities/iosAndroidDim';
 
 export default StyleSheet.create({
-   sfvContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor:'#131833'
-  },
-  content:{
-    backgroundColor:'#131833',
-    marginHorizontal:10,
-    paddingHorizontal:10,
-    paddingVertical:5,
-    borderRadius:3
-  },
-  carousel: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  groupA: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-  },
-  image: {
-     width: getDimensions().width,
-     height:  getDimensions().height * 0.75,
-  },
-  switch: {
-    marginBottom: '5%'
-  },
-  hideTutorial: {
-    marginRight: '5%',
-    color: 'white',
-    paddingBottom: '10%'
-
-  },
-  switchContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent:'flex-start',
-    alignItems: 'center'
-  },
-  text: {
-    marginTop: '2%',
-    marginBottom: '10%',
-    color: '#36E5CE'
-  }
-})
+    sfvContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor:'#131833'
+    },
+    text: {
+        marginTop: '2%',
+        marginBottom: '10%',
+        color: '#36E5CE'
+    },
+    progressContainer: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        position: 'absolute',
+        bottom: 16
+    },
+    progressRow: {
+        width: getDimensions().width / 3
+    },
+    progressCircleIndicator: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        borderRadius: 100,
+        marginRight: 11,
+        width: 9,
+        height: 9
+    },
+    finishTextButton: {
+        fontSize: 14,
+        letterSpacing: .5,
+        color: '#3DF9DF',
+        textAlign: 'right',
+        marginRight: 16
+    }
+});

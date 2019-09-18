e65a09aba0ca525ba5acd18fb5cc9cfe7741a124
DHVSGG <diego@qapla.gg>
2019-09-18 09:09:08 -0500

    Logros screens created

---
+++ src/screens/LogrosActivosScreen/LogrosActivosScreen.js
@@ -0,0 +1,14 @@
+// diego           - 18-09-2019 - us110 - File creation
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export class LogrosActivosScreen extends Component {
+    render() {
+        return (
+            <Text>Logros activos</Text>
+        );
+    }
+}
+
+export default LogrosActivosScreen;
---
+++ src/screens/LogrosCompletadosScreen/LogrosCompletadosScreen.js
@@ -0,0 +1,14 @@
+// diego           - 18-09-2019 - us110 - File creation
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export class LogrosCompletadosScreen extends Component {
+    render() {
+        return (
+            <Text>Logros completados</Text>
+        );
+    }
+}
+
+export default LogrosCompletadosScreen;

ee849876696a8e6cd8ab60966016df7ee8931824
DHVSGG <diego@qapla.gg>
2019-09-19 12:53:11 -0500

    ProgressStepsIndicator created

---
+++ src/components/ProgressStepsIndicator/ProgressStepsIndicator.js
@@ -0,0 +1,24 @@
+// diego           - 19-09-2019 - us121 - File creation
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import styles from './style';
+
+export class ProgressStepsIndicator extends Component {
+    render() {
+        let steps = [];
+        for (let index = 0; index < this.props.steps; index++) {
+            steps.push(
+                <View style={[styles.indicator, { backgroundColor: index === this.props.selected ? '#3DF9DF' : '#060919'  }]} />
+            );
+        }
+        return (
+            <View style={styles.container}>
+                {steps}
+            </View>
+        );
+    }
+}
+
+export default ProgressStepsIndicator;
---
+++ src/components/ProgressStepsIndicator/style.js
@@ -0,0 +1,16 @@
+// diego           - 19-09-2019 - us121 - File creation
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        marginTop: 8
+    },
+    indicator: {
+        width: 10,
+        height: 10,
+        borderRadius: 100,
+        marginRight: 8
+    }
+});

58ffbda098ec26f5e5a04a327ace7f06372233a8
Josep Maria <jtomas.sanahuja@gmail.com>
2019-09-25 10:08:55 -0500

    added navigation to external link

--- src/components/LogroCard/LogroSocial.js
+++ src/components/LogroCard/LogroSocial.js
@@ -1,8 +1,9 @@
+// josep.sanahuja - 26-09-2019 - us117 - Added goToSocialLink
 // josep.sanahuja - 20-09-2019 - us111 - Added disabledContainer logic
 // josep.sanahuja - 19-09-2019 - us114 - File creation
 
 import React, { Component } from 'react';
-import { View, Image, TouchableWithoutFeedback, Text } from 'react-native';
+import { View, Image, TouchableWithoutFeedback, Text, Linking } from 'react-native';
 
 import styles from './style';
 import Images from '../../../assets/images';
@@ -10,6 +11,24 @@ import Images from '../../../assets/images';
 const QaploinIcon = Images.svg.qaploinsIcon;
 
 class LogroSocial extends Component {
+    /**
+     * Validates that the url is a valid url
+     */
+    urlIsValid() {
+        /**
+         * Reg. exp to check for valid url's
+         * Lear more about reg exp in: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions
+         */
+        return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(this.state.url);
+    }
+
+    /**
+     * Send the user to the social link page (if app is installed then asked to open with the app)
+     */
+    goToSocialLink = (url) => {
+        Linking.openURL(url);
+    }
+
     render() {
         const {titulo, descripcion, qaploins, photoUrl, pageLink, totalPuntos, verified} = this.props;
         
@@ -38,7 +57,7 @@ class LogroSocial extends Component {
                     </View>
                 </View>
                 <View style={styles.shareContainer}>
-                    <TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback onPress={() => {this.goToSocialLink(pageLink)}}>
                         <View>
                             <Text style={styles.likeText}>Dar Like</Text>
                         </View>

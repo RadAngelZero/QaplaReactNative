b476c7303bd7e6a055b157d91df19ed2770fa7b8
Josep Maria <jtomas.sanahuja@gmail.com>
2019-10-24 00:45:51 -0500

    AddGamerTagModal style.js

--- src/components/ActivityNotificationCard/style.js
+++ src/components/ActivityNotificationCard/style.js
@@ -3,7 +3,6 @@
 import { StyleSheet } from 'react-native';
 import { heightPercentageToPx, widthPercentageToPx } from '../../utilities/iosAndroidDim'
 
-
 export default styles = StyleSheet.create({
     container: {
         height: heightPercentageToPx(14),

ad589b66da19ab88dd444f72d9d56c655ab74200
Josep Maria <jtomas.sanahuja@gmail.com>
2019-10-24 01:00:41 -0500

    AnnouncementsScrollView style.js

--- src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
+++ src/screens/PublicMatchesFeedScreen/PublicMatchesFeedScreen.js
@@ -140,11 +140,11 @@ class PublicMatchesFeedScreen extends Component {
     componentDidMount() {
         this.checkHighlightsFlags();  
 
-        console.log('Dimensions height: ' +
-            28 + ' : ' + getPercentHeight(28)
+        console.log('Dimensions height: '
+            // 28 + ' : ' + getPercentHeight(28)
         );
         console.log('Dimensions width: ' +
-            36 + ' :  ' + getPercentWidth(36) + '\n'
+            6 + ' :  ' + getPercentWidth(6) + '\n'
             // 18 + ' :  ' + getPercentWidth(18)
         );
     }

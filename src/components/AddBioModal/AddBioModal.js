bcf4ef1fe96043f772c2a8288b50979cbfc320f7
DHVSGG <diego@qapla.gg>
2019-11-20 10:44:25 -0600

    AddBioModal created

---
+++ src/components/AddBioModal/AddBioModal.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Modal, View, TouchableWithoutFeedback, Text, TextInput } from 'react-native';
+import { connect } from 'react-redux';
+
+import styles from './style';
+import Images from './../../../assets/images';
+import { updateUserBio } from '../../services/database';
+
+const CloseIcon = Images.svg.closeIcon;
+
+class AddBioModal extends Component {
+    state = {
+        bio: '',
+        selected: false
+    };
+
+    /**
+     * Toggle the selection of the TextInput so the user can know when the text input is selected
+     * (we show the cool border bottom color when selected is true)
+     */
+    toggleInputSelection = () => {
+        this.setState({ selected: !this.state.selected });
+    }
+
+    /**
+     * Clean the local state and then close the modal
+     */
+    closeModal = () => {
+        this.setState({ Bio: '', selected: false });
+        this.props.onClose();
+    }
+
+    /**
+     * Call to the database function to update the biography of the user and then close the modal
+     */
+    updateBio = () => {
+        updateUserBio(this.props.uid, this.state.bio);
+        this.closeModal();
+    }
+
+    render() {
+        return (
+            <Modal
+                animationType='fade'
+                transparent={true}
+                visible={this.props.open}
+                onRequestClose={this.props.onClose}>
+                <View style={styles.mainContainer}>
+                    <View style={styles.container}>
+                        <TouchableWithoutFeedback onPress={this.closeModal}>
+                            <View style={styles.closeIcon}>
+                                <CloseIcon />
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <Text style={styles.modalTitle}>Agrega tu biografía</Text>
+                        <TextInput
+                            onFocus={this.toggleInputSelection}
+                            onBlur={this.toggleInputSelection}
+                            placeholder='Inserta tu biografía'
+                            placeholderTextColor='#B5B5B5'
+                            defaultValue={this.props.bio}
+                            style={[styles.qaplaTextInput, { borderBottomColor: this.state.selected ? '#3DF9DF' : '#B5B5B5' } ]}
+                            onChangeText={(bio) => this.setState({ bio })} />
+                        <View style={styles.buttonsContainer}>
+                            <TouchableWithoutFeedback onPress={this.closeModal}>
+                                <View style={styles.cancelTextButton}>
+                                    <Text style={styles.textOfButtons}>Cancelar</Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                            <TouchableWithoutFeedback onPress={this.updateBio}>
+                                <View style={styles.saveTextButton}>
+                                    <Text style={styles.textOfButtons}>Listo</Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        uid: state.userReducer.user.id,
+        bio: state.userReducer.user.bio
+    }
+}
+
+export default connect(mapStateToProps)(AddBioModal);
---
+++ src/components/AddBioModal/style.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from 'react-native';
+import { heightPercentageToPx, widthPercentageToPx } from '../../utilities/iosAndroidDim';
+
+export default styles = StyleSheet.create({
+    mainContainer: {
+		flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        width: widthPercentageToPx(100),
+        height: heightPercentageToPx(100),
+    },
+    container: {
+        width: widthPercentageToPx(80),
+        alignSelf: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#141833',
+        borderRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: widthPercentageToPx(0),
+            height: heightPercentageToPx(1.48),
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 24,
+        alignItems: 'center'
+    },
+    closeIcon: {
+        marginRight: widthPercentageToPx(5.33),
+        marginBottom: heightPercentageToPx(2.46),
+        marginTop: heightPercentageToPx(2.46),
+        alignSelf: 'flex-end'
+    },
+    modalTitle: {
+        color: '#FFF',
+        fontSize: 24,
+        maxWidth: widthPercentageToPx(70)
+    },
+    qaplaTextInput: {
+        color: '#FFF',
+        marginTop: heightPercentageToPx(1.97),
+        width: widthPercentageToPx(70),
+        borderBottomWidth: 2
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        alignSelf: 'flex-end',
+        marginTop: heightPercentageToPx(1.97)
+    },
+    cancelTextButton: {
+        marginTop: heightPercentageToPx(1.97),
+        marginBottom: heightPercentageToPx(1.97),
+        alignSelf: 'flex-end'
+    },
+    textOfButtons: {
+        color: '#3DF9DF',
+        margin: heightPercentageToPx(1.97)
+    },
+    saveTextButton: {
+        marginTop: heightPercentageToPx(1.97),
+        marginBottom: heightPercentageToPx(1.97),
+        marginRight: heightPercentageToPx(1.97),
+        alignSelf: 'flex-end'
+    }
+});

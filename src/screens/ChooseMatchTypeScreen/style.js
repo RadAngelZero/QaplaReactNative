c6aac0896a098f316313eb2e9b2f4f3230d6f274
DHVSGG <diego@qapla.gg>
2020-01-03 13:21:15 -0600

    Updated ChooseMatchScreeen according to supernova mock

--- src/screens/ChooseMatchTypeScreen/style.js
+++ src/screens/ChooseMatchTypeScreen/style.js
@@ -7,20 +7,21 @@ import { heightPercentageToPx, widthPercentageToPx } from '../../utilities/iosAn
 export default styles = StyleSheet.create({
     sfvContainer: {
         flex: 1,
-        justifyContent: 'center',
-        backgroundColor:'#131833'
+        alignItems: "center",
+        backgroundColor:'#0d1021'
     },
     container: {
-        flex: 1,
-        backgroundColor:'#131833'
+        flex: 1
     },
     titleText: {
         color: '#FFF',
         marginLeft: widthPercentageToPx(6.4),
         fontSize: 32,
         fontWeight: 'bold',
-        width: widthPercentageToPx(60),
-        marginTop: heightPercentageToPx(5)
+        width: widthPercentageToPx(70),
+        lineHeight: 38,
+        letterSpacing: 0.51,
+        alignSelf: "flex-start",
     },
     lightningImage: {
         marginTop: heightPercentageToPx(8.74),
@@ -28,7 +29,7 @@ export default styles = StyleSheet.create({
     },
     publicMatchButton: {
         marginTop: heightPercentageToPx(8.13),
-        borderRadius: 100,
+        borderRadius: 30,
         backgroundColor: '#FA2D79',
         elevation: 6,
         shadowColor: "#000",
@@ -43,10 +44,11 @@ export default styles = StyleSheet.create({
     },
     publicMatchButtonText: {
         color: '#FFF',
-        fontSize: 14,
+        fontSize: 17,
         fontWeight: 'bold',
         letterSpacing: .5,
         textAlign: 'center',
+        lineHeight: 22,
         marginTop: heightPercentageToPx(2.46),
         marginBottom: heightPercentageToPx(2.46),
         marginLeft: widthPercentageToPx(8.53),
@@ -71,13 +73,14 @@ export default styles = StyleSheet.create({
     },
     directMatchButtonText: {
         color: '#FFF',
-        fontSize: 14,
+        fontSize: 17,
         fontWeight: 'bold',
         letterSpacing: .5,
+        lineHeight: 22,
         textAlign: 'center',
         marginTop: heightPercentageToPx(2.46),
         marginBottom: heightPercentageToPx(2.46),
-        marginLeft: widthPercentageToPx(2.13),
+        marginLeft: widthPercentageToPx(2.26),
         marginRight: widthPercentageToPx(8.53)
     }
 });

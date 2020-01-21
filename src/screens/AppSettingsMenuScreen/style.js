0aaa5300d065c4b26573547bbca0e3b54c58492f
DHVSGG <diego@qapla.gg>
2020-01-21 16:48:06 -0600

    Function for android notch devices implemented

--- src/screens/AppSettingsMenuScreen/style.js
+++ src/screens/AppSettingsMenuScreen/style.js
@@ -1,13 +1,14 @@
 // josep.sanahuja  - 13-11-2019 - us147 - File creation
 
 import { StyleSheet } from 'react-native';
-import { heightPercentageToPx, widthPercentageToPx } from '../../utilities/iosAndroidDim'
+import { heightPercentageToPx, widthPercentageToPx, paddingTopForAndroidDevicesWithNotch } from '../../utilities/iosAndroidDim'
 
 export default styles = StyleSheet.create({
     sfvContainer: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor:'#131833'
+        backgroundColor:'#131833',
+        paddingTop: paddingTopForAndroidDevicesWithNotch()
     },
     container: {
         flex: 1,

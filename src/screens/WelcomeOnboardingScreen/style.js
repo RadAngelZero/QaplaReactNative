f73484398c803b993d0b35d4eaf4dd2a9069f707
DHVSGG <diego@qapla.gg>
2020-01-29 17:48:55 -0600

    Make automatic scroll to allow any user to go to the final screen

--- src/screens/WelcomeOnboardingScreen/style.js
+++ src/screens/WelcomeOnboardingScreen/style.js
@@ -34,7 +34,7 @@ export default StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
         borderRadius: 100,
-        marginRight: widthPercentageToPx(11.29),
+        marginRight: 16,
         width: widthPercentageToPx(2.4),
         height:  widthPercentageToPx(2.4)
     },

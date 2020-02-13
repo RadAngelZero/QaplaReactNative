2859133678bd38ebbc39be115455c3c5fab7677c
DHVSGG <diego@qapla.gg>
2020-02-13 14:37:04 -0600

    Router refactor

--- src/Router.js
+++ src/Router.js
@@ -63,314 +63,328 @@ const ProfileIcon = Images.svg.profileIcon;
 const PublicFeedMatchIcon = Images.svg.publicFeedMatchIcon;
 const LogrosIcon = Images.svg.logrosIcon;
 
-const LogrosTabNavigator = createMaterialTopTabNavigator(
-  {
-    LogrosActivos: {
-      screen: LogrosActivosScreen,
-      navigationOptions: () => ({
-        title: translate('router.topNavigators.achievements.active')
-      })
-    },
-    LogrosCompletados: {
-      screen: LogrosCompletadosScreen,
-      navigationOptions: () => ({
-        title: translate('router.topNavigators.achievements.completed')
-      })
-    }
-  },
-  {
-    initialRouteName: 'LogrosActivos',
-    tabBarOptions: {
-      upperCaseLabel: false,
-      style: {
-        backgroundColor: '#0C1021'
-      },
-      tabStyle: {
-        width: widthPercentageToPx(36)
-      },
-      labelStyle: {
-        fontSize: 14
-      },
-      activeTintColor: '#FFF',
-      inactiveTintColor: '#FFF',
-      indicatorStyle: {
-        borderBottomColor: '#36E5CE',
-        borderBottomWidth: 2,
-        width: widthPercentageToPx(36)
-      }
-    }
-  }
-);
+//#region Stack Navigators
 
-const NotificationTabNavigator = createMaterialTopTabNavigator(
-  {
-    NotificationActividad: {
-      screen: ActivityNotificationsScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: translate('router.topNavigators.notifications.activity')
-      })
-    },
-    NotificationRetas: {
-      screen: RetasNotificationsScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: translate('router.topNavigators.notifications.matches'),
-        tabBarIcon: ({ tintColor, focused }) => (
-          <BadgeForNotificationTab />
-        )
-      })
+const CheckOutStackNavigator = createStackNavigator({
+    CheckOut: {
+        screen: CheckOutPaymentScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
     }
-  },
-  {
-    initialRouteName: 'NotificationActividad',
-    tabBarOptions: {
-      upperCaseLabel: false,
-      style: {
-        backgroundColor: '#0C1021'
-      },
-      tabStyle: {
-        width: widthPercentageToPx(35)
-      },
-      labelStyle: {
-        fontSize: 14
-      },
-      activeTintColor: '#FFF',
-      inactiveTintColor: '#FFF',
-      indicatorStyle: {
-        borderBottomColor: '#36E5CE',
-        borderBottomWidth: 2,
-        width: widthPercentageToPx(35)
-      }
-    },
-  }
-);
-
-const RetasTabNavigator = createMaterialTopTabNavigator(
-  {
-    Publicas: {
-      screen: PublicMatchesFeedScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: translate('router.topNavigators.matches.public')
-      })
-    },
-    MisRetas: {
-      screen: MyMatchesScreen,
-      navigationOptions: ({ navigation }) => ({
-        title: translate('router.topNavigators.matches.myMatches')
-      })
-    }
-  },
-  {
-    initialRouteName: 'Publicas',
-    tabBarOptions: {
-      upperCaseLabel: false,
-      style: {
-        backgroundColor: '#0C1021'
-      },
-      tabStyle: {
-        width: widthPercentageToPx(35)
-      },
-      labelStyle: {
-        fontSize: 14
-      },
-      activeTintColor: '#FFF',
-      inactiveTintColor: '#FFF',
-      indicatorStyle: {
-        borderBottomColor: '#36E5CE',
-        borderBottomWidth: 2,
-        width: widthPercentageToPx(35)
-      }
-    },
-  }
-);
-
-const TabMainNavigator = createBottomTabNavigator({
-  Logros: {
-    screen: LogrosTabNavigator,
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor, focused }) => (
-        <View>
-          <LogrosIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'} />
-          <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.events')}</Text>
-        </View>
-      )
-    })
-  },
-  Retas: {
-    screen:   RetasTabNavigator,
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor, focused }) => (
-        <View>
-          <PublicFeedMatchIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'} />
-            <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.matches')}</Text>
-        </View>
-      )
-    })
-  },
-  Perfil: {
-    screen:   UserProfileScreen,
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor, focused }) => (
-        <View>
-          <ProfileIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'}/>
-            <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.profile')}</Text>
-        </View>
-      )
-    })
-  }
-},
-{
-  tabBarOptions: {
-    style: {
-      backgroundColor: '#0C1021',
-      height: 60, 
-      padding:0, 
-      margin:0, 
-      borderTopColor: 'transparent'
-    },
-    showLabel: false,
-    activeTintColor: '#36E5CE',
-    tabStyle: {
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-  }
 });
 
-const AppWithHeaderStackNavigator = createStackNavigator(
-  {
-    HomeScreen: {
-      screen: TabMainNavigator
-    }
-  },
-  {
-    initialRouteName: 'HomeScreen',
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false,
-    }
-  }
-);
-
-const MatchWizardStackNavigator = createStackNavigator(
-  {
+const MatchWizardStackNavigator = createStackNavigator({
     ChooseMatchType: {
-      screen: ChooseMatchTypeScreen,
-      navigationOptions: {
-        header: props => <TopNavOptions close {...props} onCloseGoTo='Publicas' />
-      }
+        screen: ChooseMatchTypeScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
     },
-    LoadGames: {
-      screen: LoadGamesScreen
+    SelectGame: {
+        screen: LoadGamesScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions back close {...props} />
+        }
     },
-    ChooseOponent: {
-      screen: ChooseOpponentScreen,
-      navigationOptions: {
-        header: null
-      }
-    },
-    SetBet: {
-      screen: SetBetScreen
-    },
-    CheckOut: {
-      screen: CheckOutPaymentScreen
-    }
-  },
-  {
-    initialRouteName: 'ChooseMatchType',
-  }
-);
+    SetBet: SetBetScreen
+});
 
-const AppNoHeaderStackNavigator = createSwitchNavigator(
-  {
-    MatchWizard: {
-      screen: MatchWizardStackNavigator
+const MatchDetailsStackNavigator = createStackNavigator({
+    MatchDetails: {
+        screen: PublicMatchCardScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
     },
     UploadMatchResult: {
-      screen: UploadMatchResultScreen
+        screen: UploadMatchResultScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
     }
-  }
+});
+
+const SettingsMenuStackNavigator = createStackNavigator({
+    AppSettingsMenu: {
+        screen: AppSettingsMenuScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
+    },
+    Support: {
+        screen: SupportScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions back close {...props} />
+        }
+    }
+});
+
+const AddGameStackNavigator = createStackNavigator({
+    AddGame: {
+        screen: LoadGamesScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
+    }
+});
+
+const VerificationStackNavigator = createStackNavigator({
+    Verification: VerificationScreen
+},{
+    headerMode: 'none'
+});
+
+const AuthStackNavigator = createStackNavigator({
+    SignIn: {
+        screen: SignInScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions close {...props} />
+        }
+    },
+    LogIn: {
+        screen: LoginWithEmailScreen,
+        navigationOptions: {
+            header: (props) => <TopNavOptions back close {...props} />
+        }
+    }
+},{
+    navigationOptions: {
+        gesturesEnabled: false
+    }
+});
+
+//#endregion
+
+//#region Top Tab Navigators
+
+const MatchesTopTabNavigator = createMaterialTopTabNavigator(
+    {
+      Public: {
+        screen: PublicMatchesFeedScreen,
+        navigationOptions: {
+          title: translate('router.topNavigators.matches.public')
+        }
+      },
+      MyMatches: {
+        screen: MyMatchesScreen,
+        navigationOptions: {
+          title: translate('router.topNavigators.matches.myMatches')
+        }
+      }
+    },
+    {
+      tabBarOptions: {
+        upperCaseLabel: false,
+        style: {
+          backgroundColor: '#0C1021'
+        },
+        tabStyle: {
+          width: widthPercentageToPx(35)
+        },
+        labelStyle: {
+          fontSize: 14
+        },
+        activeTintColor: '#FFF',
+        inactiveTintColor: '#FFF',
+        indicatorStyle: {
+          borderBottomColor: '#36E5CE',
+          borderBottomWidth: 2,
+          width: widthPercentageToPx(35)
+        }
+      },
+    }
 );
+
+const AchievementsTopTabNavigator = createMaterialTopTabNavigator(
+    {
+      LogrosActivos: {
+        screen: LogrosActivosScreen,
+        navigationOptions: () => ({
+          title: translate('router.topNavigators.achievements.active')
+        })
+      },
+      LogrosCompletados: {
+        screen: LogrosCompletadosScreen,
+        navigationOptions: () => ({
+          title: translate('router.topNavigators.achievements.completed')
+        })
+      }
+    },
+    {
+      initialRouteName: 'LogrosActivos',
+      tabBarOptions: {
+        upperCaseLabel: false,
+        style: {
+          backgroundColor: '#0C1021'
+        },
+        tabStyle: {
+          width: widthPercentageToPx(36)
+        },
+        labelStyle: {
+          fontSize: 14
+        },
+        activeTintColor: '#FFF',
+        inactiveTintColor: '#FFF',
+        indicatorStyle: {
+          borderBottomColor: '#36E5CE',
+          borderBottomWidth: 2,
+          width: widthPercentageToPx(36)
+        }
+      }
+    }
+);
+
+const NotificationsTopTabNavigator = createMaterialTopTabNavigator(
+    {
+      NotificationActividad: {
+        screen: ActivityNotificationsScreen,
+        navigationOptions: {
+          title: translate('router.topNavigators.notifications.activity')
+        }
+      },
+      NotificationRetas: {
+        screen: RetasNotificationsScreen,
+        navigationOptions: {
+          title: translate('router.topNavigators.notifications.matches'),
+          tabBarIcon: ({ tintColor, focused }) => (
+            <BadgeForNotificationTab />
+          )
+        }
+      }
+    },
+    {
+        initialRouteName: 'NotificationActividad',
+        navigationOptions: {
+            header: (props) => <NotificationsHeader {...props} />
+        },
+        tabBarOptions: {
+            upperCaseLabel: false,
+            style: {
+            backgroundColor: '#0C1021'
+            },
+            tabStyle: {
+            width: widthPercentageToPx(35)
+            },
+            labelStyle: {
+            fontSize: 14
+            },
+            activeTintColor: '#FFF',
+            inactiveTintColor: '#FFF',
+            indicatorStyle: {
+            borderBottomColor: '#36E5CE',
+            borderBottomWidth: 2,
+            width: widthPercentageToPx(35)
+            }
+        },
+    }
+);
+
+//#endregion
+
+//#region Bottom Tab Navigator
+
+const MainBottomTabNavigator = createBottomTabNavigator({
+        Achievements: {
+            screen: AchievementsTopTabNavigator,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor, focused }) => (
+                    <View>
+                    <LogrosIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'} />
+                    <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.events')}</Text>
+                    </View>
+                )
+            }
+        },
+        Matches: {
+            screen: MatchesTopTabNavigator,
+            navigationOptions:{
+                tabBarIcon: ({ tintColor, focused }) => (
+                    <View>
+                    <PublicFeedMatchIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'} />
+                        <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.matches')}</Text>
+                    </View>
+                )
+            }
+        },
+        Profile: {
+            screen: UserProfileScreen,
+            navigationOptions:{
+                tabBarIcon: ({ tintColor, focused }) => (
+                    <View>
+                    <ProfileIcon width={25} height={25} style={{ alignSelf: 'center' }} color={focused ? '#36E5CE' : '#FFF'}/>
+                        <Text style={{ color: focused? '#36E5CE' : '#FFF', fontSize: 12, lineHeight: 14 }}>{translate('router.bottomNavigators.mainNavigator.profile')}</Text>
+                    </View>
+                )
+            }
+        }
+    }, {
+        tabBarOptions: {
+            style: {
+              backgroundColor: '#0C1021',
+              height: 60,
+              padding:0,
+              margin:0,
+              borderTopColor: 'transparent'
+            },
+            showLabel: false,
+            activeTintColor: '#36E5CE',
+            tabStyle: {
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }
+        }
+    }
+);
+
+//#endregion
+
+//#region Root Stack Navigator
+
+const RootStackNavigator = createStackNavigator({
+    MainBottomNavigator: {
+        screen: MainBottomTabNavigator,
+        navigationOptions: {
+            header: (props) => <HeaderBar {...props} />
+        }
+    },
+    Notifications: {
+        screen: NotificationsTopTabNavigator,
+        navigationOptions: {
+            header: (props) => <NotificationsHeader {...props} />
+        }
+    },
+    MatchWizard: MatchWizardStackNavigator,
+    MatchDetails: MatchDetailsStackNavigator,
+    SettingsMenu: SettingsMenuStackNavigator,
+    AddGame: AddGameStackNavigator,
+    Verification: VerificationStackNavigator,
+    CheckOut: CheckOutStackNavigator,
+    Auth: AuthStackNavigator
+},{
+    defaultNavigationOptions: {
+        header: null
+    }
+});
+
+//#endregion
+
+//#region Switch Navigator
+
+const MainSwitchNavigator = createSwitchNavigator({
+    SplashScreen: AuthLoadingScreen,
+    App: RootStackNavigator,
+    onBoarding: WelcomeOnboardingScreen,
+    ChooseUserName: ChooseUserNameScreen
+});
+
+//#endregion
 
 class Router extends React.Component {
   render() {
-    // or not shown
-    const RootStack = createStackNavigator(
-      {
-        SignIn: {
-          screen: SignInScreen,
-          navigationOptions: {
-            header: null
-          }
-        },
-        Login: {
-          screen: LoginWithEmailScreen
-        },
-        Home: {
-          screen: AppWithHeaderStackNavigator,
-          navigationOptions: {
-            header: props => <HeaderBar {...props} />
-          }
-        },
-        NoHeader: {
-          screen: AppNoHeaderStackNavigator,
-          navigationOptions: {
-            header: null
-          }
-        },
-        AppSettingsMenu:{
-          screen: AppSettingsMenuScreen,
-          navigationOptions: {
-            header: props => <TopNavOptions back {...props} />
-          }
-        },
-        Notifications: {
-          screen: NotificationTabNavigator,
-          navigationOptions: {
-            header: props => <NotificationsHeader {...props} />
-          }
-        },
-        MatchCard: {
-          screen: PublicMatchCardScreen
-        },
-        Support: {
-          screen: SupportScreen,
-          navigationOptions: {
-            header: props => <TopNavOptions back {...props} />
-          }
-        },
-        Verification:{
-          screen: VerificationScreen,
-          navigationOptions: {
-            header: null
-          }
-        },
-        ChooseUserNameScreen: {
-          screen: ChooseUserNameScreen,
-          navigationOptions: {
-            header: null
-          }
-        }
-      },
-      {
-        initialRouteName:  'Home',
-        headerMode: 'screen'
-      }
-    );
-
-    const MainNavigator = createSwitchNavigator(
-      {
-        AuthLoadingScreen: AuthLoadingScreen,
-        App: RootStack,
-        Welcome: WelcomeOnboardingScreen
-      },
-      {
-        initialRouteName: 'AuthLoadingScreen'
-      }
-    );
-
     // Create main router entry point for the app
-    const AppContainer = createAppContainer(MainNavigator);
+    const AppContainer = createAppContainer(MainSwitchNavigator);
 
     /**
      * @description

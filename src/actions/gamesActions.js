b06418017861b0f660cce35b8e43596d53ed351e
DHVSGG <diego@qapla.gg>
2020-03-06 16:28:05 -0600

    Doc added

--- src/actions/gamesActions.js
+++ src/actions/gamesActions.js
@@ -12,9 +12,26 @@ export const getListOfGames = () => async (dispatch) => {
     var listOfGames = {};
     const listOfPlatformsWithGames = await gamesRef.once('value');
 
+    /**
+     * We load all the games from the server with the structure:
+     * platformName_white: {
+     *  gameName: {
+     *      gameData
+     *  }
+     * }
+     * After that we check what games media (images and icons) we have on the local, so we do not
+     * call the images and icons from the server, but we use the string data like name, acronym, etc.
+     * because this data can change (for example FIFA, every year changes the name Fifa 20 -> Fifa 21)
+     * and we do not want to upload the aplication for that kind of things
+     */
     listOfPlatformsWithGames.forEach((platform) => {
         listOfGames[platform.key] = platform.val();
         platform.forEach((game) => {
+            /**
+             * If we have the game media on the local we replaces the icon and image field with references to the
+             * local icon and image and we set the local flag to true, so we can render the image correctly on the
+             * componentes
+             */
             if (gamesResources[platform.key] && gamesResources[platform.key][game.key]){
                 listOfGames[platform.key][game.key].icon = gamesResources[platform.key][game.key].icon;
                 listOfGames[platform.key][game.key].image = gamesResources[platform.key][game.key].image;

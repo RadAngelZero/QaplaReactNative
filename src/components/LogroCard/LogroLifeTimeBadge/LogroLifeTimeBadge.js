4aaf5882c050aed80f9c6e544d1d90a08ecc0a0b
DHVSGG <diego@qapla.gg>
2020-03-20 09:00:51 -0600

    Update LogroLifeTimeBadge time calculus algorhitm

--- src/components/LogroCard/LogroLifeTimeBadge/LogroLifeTimeBadge.js
+++ src/components/LogroCard/LogroLifeTimeBadge/LogroLifeTimeBadge.js
@@ -6,8 +6,6 @@ import { ONE_HOUR_MILISECONDS, HOURS_IN_DAY } from './../../../utilities/Constan
 
 export class LogroLifeTimeBadge extends Component {
     render() {
-        const dayObject = new Date();
-        
         /**
          * The date come from the cards in the next format:
          * Day-Month-Year
@@ -15,28 +13,47 @@ export class LogroLifeTimeBadge extends Component {
          * the original format is not valide to create a date object
          */
         const [day, month, year] = this.props.limitDate.split('-');
-        const logroEndDate = new Date(`${month}/${day}/${year}`);
+        const [hour, minutes] = this.props.hour.split(':');
+        const logroEndDate = new Date(`${month}/${day}/${year} ${hour}:${minutes}`);
+        const mexicoDate = new Date().toLocaleDateString('en-us', { timeZone: 'America/Mexico_City', year: '2-digit' });
+        const [currentMonth, currentDay, currentYear] = mexicoDate.split('/');
+        const mexicoHour = new Date().toLocaleTimeString('en-us', { timeZone: 'America/Mexico_City', hour12: false });
+        const [currentHour, currentMinutes] = mexicoHour.split(':');
         const currentDate = new Date(
-            `${dayObject.getUTCMonth() + 1}/${dayObject.getUTCDate()}/${dayObject.getUTCFullYear()} ${dayObject.getUTCHours()}:${dayObject.getUTCMinutes()}`
+            `${currentMonth}/${currentDay}/${currentYear} ${currentHour}:${currentMinutes}`
         );
 
         let leftHours = (logroEndDate.getTime() - currentDate.getTime()) / ONE_HOUR_MILISECONDS;
         const remainingDays = Math.round(leftHours / HOURS_IN_DAY);
-        const remainingHours = Math.round(leftHours - (remainingDays * HOURS_IN_DAY));
+        const remainingHours = Math.trunc(leftHours - (remainingDays * HOURS_IN_DAY));
+
+        let remainingMinutes = null;
+        if (remainingDays === 0 && remainingHours === 0) {
+            remainingMinutes = parseInt(hour) > parseInt(currentHour) ? (60 + parseInt(minutes)) - parseInt(currentMinutes) : parseInt(minutes) - parseInt(currentMinutes);
+        }
 
         return (
-            <View style={styles.timeLifeBadge}>
-                {remainingDays > 0 ?
-                    <Text style={styles.timeLife}>
-                        {`${remainingDays}d ${remainingHours}h`}
-                    </Text>
+            <>
+                {/**
+                 * If the event has not begined yet, we show this component, otherwise we return null
+                 */}
+                {(remainingDays > 0 || remainingHours >= 0) && (!remainingMinutes || remainingMinutes >= 0) ?
+                    <View style={styles.timeLifeBadge}>
+                        {remainingDays > 0 ?
+                            <Text style={styles.timeLife}>
+                                {`${remainingDays}d ${remainingHours > 0 ? `${remainingHours}h` : ''}`}
+                            </Text>
+                            :
+                            remainingDays === 0 &&
+                            <Text style={styles.timeLife}>
+                                {`${remainingHours > 0 ? `${remainingHours}h` : remainingHours === 0 ? `${remainingMinutes}m` : ''}`}
+                            </Text>
+                        }
+                    </View>
                     :
-                    remainingDays === 0 &&
-                    <Text style={styles.timeLife}>
-                        {`${remainingHours}h`}
-                    </Text>
+                    null
                 }
-            </View>
+            </>
         );
     }
 }

3993267a82aee1e29d00820f20787ef22d18be1e
DHVSGG <diego@qapla.gg>
2020-03-24 16:35:14 -0600

    Get anouncement on different languages

--- src/components/AnnouncementsScrollView/AnnouncementsScrollView.js
+++ src/components/AnnouncementsScrollView/AnnouncementsScrollView.js
@@ -3,9 +3,9 @@
 import React, { Component } from 'react';
 import { View, ScrollView } from 'react-native';
 import styles from './style';
-import Images from '../../../assets/images';
 import AnnouncementCard from '../AnnouncementCard/AnnouncementCard';
 import { getAnnouncements } from '../../services/database';
+import { getLocaleLanguage } from './../../utilities/i18';
 
 class AnnouncementsScrollView extends Component {
     constructor(props) {
@@ -21,21 +21,21 @@ class AnnouncementsScrollView extends Component {
     }
 
     async getlistOfAnnouncements() {
-        const res = await getAnnouncements();
+        const announcements = await getAnnouncements();
         this.setState({
-            announcements: res
+            announcements
         });
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <ScrollView horizontal style={styles.scrollViewStyle}>
+                <ScrollView horizontal style={styles.scrollViewStyle} showsHorizontalScrollIndicator={false}>
                     {this.state.announcements.map((ancmnt, index) => (
                         <AnnouncementCard
                             key={`Announcement-${index}`}
                             photoUrl={ancmnt.photoUrl}
-                            description={ancmnt.description}
+                            description={ancmnt.descriptions[getLocaleLanguage()]}
                         />
                     ))}
                 </ScrollView>

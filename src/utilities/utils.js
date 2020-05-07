16a6dc62efd3c1d2c5aecc1d86356d8c95bb8cb2
DHVSGG <diego@qapla.gg>
2020-05-07 17:53:08 -0500

    Date and hour functions created

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -180,3 +180,30 @@ export function withdrawQaploins() {
 export function isValidGame(platform, game) {
     return platform && store.getState().gamesReducer.games[platform] && store.getState().gamesReducer.games[platform][game];
 }
+
+/**
+ * Return the elements day, month and year of the given date
+ * @param {string} date Date with format DD-MM-YYYY
+ * @returns {Array} [day, month, year]
+ */
+export function getDateElementsAsNumber(date) {
+    let [day, month, year] = date.split('-');
+    day = parseInt(day);
+    month = parseInt(month);
+    year = parseInt(year);
+
+    return [day, month, year];
+}
+
+/**
+ * Return the elements hour and minutes of the given hour
+ * @param {string} hour Hour in format HH:MM
+ * @returns {Array} [hours, minutes]
+ */
+export function getHourElementsAsNumber(hour) {
+    let [hours, minutes] = hour.split(':');
+    hours = parseInt(hours);
+    minutes = parseInt(minutes);
+
+    return [hours, minutes]
+}

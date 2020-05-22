3e42fc7c8b335ea8d6c1d0f2a0c0ff832cd1ddbe
DHVSGG <diego@qapla.gg>
2020-05-22 17:17:35 -0500

    Validate entry on app

--- src/components/EventDetailsModal/EventRegistration.js
+++ src/components/EventDetailsModal/EventRegistration.js
@@ -9,10 +9,11 @@ import {
 import { connect } from 'react-redux';
 
 import styles from './style';
-import { sendRequestToJoinEvent, joinEvent } from '../../services/database';
+import { sendRequestToJoinEvent, joinEvent, substractQaploinsToUser } from '../../services/database';
 import { getLocaleLanguage, translate } from '../../utilities/i18';
 import { subscribeUserToTopic } from '../../services/messaging';
 import { EVENTS_TOPIC } from '../../utilities/Constants';
+import ConfirmationDialog from '../ConfirmationDialog/ConfirmationDialog';
 
 class EventRegistration extends Component {
     constructor(props) {
@@ -21,10 +22,30 @@ class EventRegistration extends Component {
         this.textInputRefs = [];
 
     	this.state = {
+            renderScreen: false,
             userData: {},
             requestError: false,
-            error: false
+            error: false,
+            openEntryDialog: false,
+            openUserDontHaveEnoughQoinsDialog: false
         };
+    }
+
+    componentDidMount() {
+        if (this.props.game && this.props.game.informationNeededToAdd) {
+            this.setState({ renderScreen: true });
+        } else {
+            this.setState({ renderScreen: false });
+            if (this.props.event.eventEntry) {
+                if (this.props.qoins >= this.props.event.eventEntry) {
+                    this.setState({ openEntryDialog: true });
+                } else {
+                    this.setState({ openUserDontHaveEnoughQoinsDialog: true });
+                }
+            } else {
+                this.saveUserRequest();
+            }
+        }
     }
 
     /**
@@ -51,6 +72,7 @@ class EventRegistration extends Component {
 
     /**
      * Validate and save the request of the user
+     * @returns {boolean} True when the request is successfully sent
      */
     saveUserRequest = async () => {
         if (this.props.game && this.props.game.informationNeededToAdd) {
@@ -80,6 +102,8 @@ class EventRegistration extends Component {
                      * Send the user to the next modal
                      */
                     this.props.goToNextStep();
+
+                    return true;
                 } catch (error) {
                     console.error(error);
                     this.setState({ requestError: true });
@@ -91,6 +115,8 @@ class EventRegistration extends Component {
                  * error to the user
                  */
                 this.setState({ error: true });
+
+                return false;
             }
         } else {
             try {
@@ -101,23 +127,83 @@ class EventRegistration extends Component {
                  */
                 subscribeUserToTopic(this.props.eventId, this.props.uid, EVENTS_TOPIC);
                 this.props.goToNextStep();
+
+                return true;
             } catch (error) {
                 console.error(error);
                 this.setState({ requestError: true });
+
+                return false;
             }
         }
     }
 
     onSubmitEditing = (index) => {
         if (index === Object.keys(this.props.game.informationNeededToAdd).length - 1) {
-            this.saveUserRequest();
+            if (this.props.event.eventEntry) {
+                this.openRightEntryDialog();
+            } else {
+                this.saveUserRequest();
+            }
         } else {
             this.textInputRefs[index + 1].focus();
         }
     }
 
+    /**
+     * Toggle the state of the entry dialog
+     */
+    toggleEntryDialog = () => this.setState({ openEntryDialog: !this.state.openEntryDialog });
+
+    /**
+     * Check if the user has enough qoins to send the request, send the request
+     * and substract the qoins from the user profile
+     */
+    validateUserEntry = async () => {
+        if (this.props.qoins >= this.props.event.eventEntry) {
+            try {
+                if (await this.saveUserRequest()) {
+                    await substractQaploinsToUser(this.props.uid, this.props.qoins, this.props.event.eventEntry);
+                }
+            } catch (error) {
+                console.error(error);
+                this.setState({ requestError: true });
+            }
+        } else {
+            this.setState({ openUserDontHaveEnoughQoinsDialog: true });
+        }
+    }
+
+    /**
+     * Toggle the state of the user dont have enough Qoins dialog
+     */
+    toggleUserDontHaveEnoughQoinsDialog = () => this.setState({ openUserDontHaveEnoughQoinsDialog: !this.state.openUserDontHaveEnoughQoinsDialog });
+
+    /**
+     * Check if the user has enough Qoins and show the entry dialog
+     * If the user do not have enough Qoins then display the user
+     * do not have enough Qoins dialog
+     */
+    openRightEntryDialog = () => {
+        if (this.props.qoins >= this.props.event.eventEntry) {
+            this.toggleEntryDialog();
+        } else {
+            this.toggleUserDontHaveEnoughQoinsDialog();
+        }
+    }
+
+    /**
+     * Cancel the registration process
+     */
+    cancelRegistration = () => {
+        this.setState({ openEntryDialog: false, openUserDontHaveEnoughQoinsDialog: false });
+        if (!this.state.renderScreen) {
+            this.props.closeModal();
+        }
+    }
+
     render() {
-        if (this.props.game && this.props.game.informationNeededToAdd) {
+        if (this.state.renderScreen) {
             const neededInformation = this.props.game.informationNeededToAdd;
             const userLanguage = getLocaleLanguage();
 
@@ -159,22 +245,43 @@ class EventRegistration extends Component {
                             style={styles.eventSponsorImageLarge} />
                         <TouchableOpacity
                             style={styles.continueButtonContainer}
-                            onPress={this.saveUserRequest}>
+                            onPress={this.props.event.eventEntry ? this.openRightEntryDialog : this.saveUserRequest}>
                             <Text style={styles.continueButtonText}>
                                 {translate('eventDetailsModal.continue')}
                             </Text>
                         </TouchableOpacity>
                     </View>
+                    <ConfirmationDialog
+                        visible={this.state.openEntryDialog}
+                        closeModal={this.toggleEntryDialog}
+                        cancel={this.cancelRegistration}
+                        accept={this.validateUserEntry}
+                        body={translate('eventDetailsModal.eventEntryDialogBody', { eventEntry: this.props.event.eventEntry, qoins: this.props.qoins })} />
+                    <ConfirmationDialog
+                        visible={this.state.openUserDontHaveEnoughQoinsDialog}
+                        closeModal={this.toggleUserDontHaveEnoughQoinsDialog}
+                        cancelButton={false}
+                        accept={this.cancelRegistration}
+                        body={translate('eventDetailsModal.notEnoughQoinsDialogBody', { eventEntry: this.props.event.eventEntry, qoins: this.props.qoins })} />
                 </View>
             );
         } else {
-            /**
-             * Maybe here we should send to event participants directly.
-             * We do not need to approve participation if we are not requesting
-             * any data?
-             */
-            this.saveUserRequest();
-            return (<></>);
+            return (
+                <>
+                    <ConfirmationDialog
+                        visible={this.state.openEntryDialog}
+                        closeModal={this.toggleEntryDialog}
+                        cancel={this.cancelRegistration}
+                        accept={this.validateUserEntry}
+                        body={translate('eventDetailsModal.eventEntryDialogBody', { eventEntry: this.props.event.eventEntry, qoins: this.props.qoins })} />
+                    <ConfirmationDialog
+                        visible={this.state.openUserDontHaveEnoughQoinsDialog}
+                        closeModal={this.toggleUserDontHaveEnoughQoinsDialog}
+                        cancelButton={false}
+                        accept={this.cancelRegistration}
+                        body={translate('eventDetailsModal.notEnoughQoinsDialogBody', { eventEntry: this.props.event.eventEntry, qoins: this.props.qoins })} />
+                </>
+            );
         }
     }
 }
@@ -183,7 +290,8 @@ function mapStateToProps(state) {
     return {
         uid: state.userReducer.user.id,
         userName: state.userReducer.user.userName,
-        token: state.userReducer.user.token
+        token: state.userReducer.user.token,
+        qoins: state.userReducer.user.credits
     }
 }
 

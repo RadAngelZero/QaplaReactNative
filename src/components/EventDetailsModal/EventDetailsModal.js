a98c45d60112aad908fbf59fe8262933619039d6
DHVSGG <diego@qapla.gg>
2021-05-06 18:16:15 -0500

    LinkTwitchAccountModal and register to event validations added

--- src/components/EventDetailsModal/EventDetailsModal.js
+++ src/components/EventDetailsModal/EventDetailsModal.js
@@ -14,31 +14,36 @@ import EventDetails from './EventDetails';
 import EventRegistration from './EventRegistration';
 import { isUserLogged } from '../../services/auth';
 import EventRegistrationSuccessful from './EventRegistrationSuccessful';
+import { userHaveTwitchId, joinEventWithCustomData, getTwitchUserName, substractQaploinsToUser } from '../../services/database';
+import LinkTwitchAccountModal from '../LinkTwitchAccountModal/LinkTwitchAccountModal';
+import { subscribeUserToTopic } from '../../services/messaging';
+import { EVENTS_TOPIC } from '../../utilities/Constants';
+import ConfirmationDialog from '../ConfirmationDialog/ConfirmationDialog';
+import { translate } from '../../utilities/i18';
 
 class EventDetailsModal extends Component {
     state = {
-        eventRegistrationStep: 0
+        eventRegistrationStep: 0,
+        showLinkWitTwitchModal: false,
+        openEntryDialog: false,
+        openUserDontHaveEnoughQoinsDialog: false
     };
 
     /**
      * Send the user to the next component
      */
     goToNextRegistrationStep = async () => {
+        console.log(this.props.uid);
         if (isUserLogged()) {
-            //Check if the user have linked the Twitch account
-            if (await userHaveTwitchId2(user.user.uid)){
-
-                if (this.scrollView) {
-                    this.scrollView.scrollTo({ y: 0, animated: false });
-                    this.setState({ eventRegistrationStep: this.state.eventRegistrationStep + 1 });
-                } else {
-                    this.props.navigation.navigate('SignIn', { streamer: this.props.events[this.props.eventId].streamerName });
-                    this.closeModal();
-                }
-                
-            }else {
-               //Create component to send the User to the Link Twitch account 
+            //Check if the user have linked their Twitch account
+            if (await userHaveTwitchId(this.props.uid)){
+                this.registerTwitchUser();
+            } else {
+               this.setState({ showLinkWitTwitchModal: true });
             }
+        } else {
+            this.props.navigation.navigate('SignIn', { streamer: this.props.events[this.props.eventId].streamerName });
+            this.closeModal();
         }
     }
 
@@ -54,6 +59,75 @@ class EventDetailsModal extends Component {
         this.setState({ eventRegistrationStep: 0 });
 
         this.props.onClose();
+    }
+
+    registerUserToEvent = async () => {
+        const twitchUserName = await getTwitchUserName(this.props.uid);
+        joinEventWithCustomData(this.props.uid, this.props.eventId, this.props.events[this.props.eventId].eventEntry, { twitchUserName });
+
+        /**
+         * Subscribe user to topic of the event
+         */
+        subscribeUserToTopic(this.props.eventId, this.props.uid, EVENTS_TOPIC);
+
+        if (this.scrollView) {
+            this.scrollView.scrollTo({ y: 0, animated: false });
+            this.setState({ eventRegistrationStep: 2 });
+        }
+    }
+
+    /**
+     * Toggle the state of the entry dialog
+     */
+    toggleEntryDialog = () => this.setState({ openEntryDialog: !this.state.openEntryDialog });
+
+    /**
+     * Check if the user has enough qoins to send the request, send the request
+     * and substract the qoins from the user profile
+     */
+    validateUserEntry = async () => {
+        if (this.props.qoins >= this.props.events[this.props.eventId].eventEntry) {
+            await this.registerUserToEvent();
+            await substractQaploinsToUser(this.props.uid, this.props.qoins, this.props.events[this.props.eventId].eventEntry);
+        } else {
+            this.setState({ openUserDontHaveEnoughQoinsDialog: true });
+        }
+    }
+
+    /**
+     * Toggle the state of the user dont have enough Qoins dialog
+     */
+    toggleUserDontHaveEnoughQoinsDialog = () => this.setState({ openUserDontHaveEnoughQoinsDialog: !this.state.openUserDontHaveEnoughQoinsDialog });
+
+    /**
+     * Check if the user has enough Qoins and show the entry dialog
+     * If the user do not have enough Qoins then display the user
+     * do not have enough Qoins dialog
+     */
+    openRightEntryDialog = () => {
+        if (this.props.qoins >= this.props.events[this.props.eventId].eventEntry) {
+            this.toggleEntryDialog();
+        } else {
+            this.toggleUserDontHaveEnoughQoinsDialog();
+        }
+    }
+
+    /**
+     * Cancel the registration process
+     */
+    cancelRegistration = () => {
+        this.setState({ openEntryDialog: false, openUserDontHaveEnoughQoinsDialog: false });
+        if (!this.state.renderScreen) {
+            this.closeModal();
+        }
+    }
+
+    registerTwitchUser = () => {
+        if (this.props.events[this.props.eventId].eventEntry) {
+            this.openRightEntryDialog();
+        } else {
+            this.registerUserToEvent();
+        }
     }
 
     render() {
@@ -97,6 +171,22 @@ class EventDetailsModal extends Component {
                         </View>
                     </ScrollView>
                 </View>
+                <LinkTwitchAccountModal
+                    open={this.state.showLinkWitTwitchModal}
+                    onClose={() => this.setState({ showLinkWitTwitchModal: false })}
+                    onLinkSuccessful={this.registerTwitchUser} />
+                <ConfirmationDialog
+                    visible={this.state.openEntryDialog}
+                    closeModal={this.toggleEntryDialog}
+                    cancel={this.cancelRegistration}
+                    accept={this.validateUserEntry}
+                    body={translate('eventDetailsModal.eventEntryDialogBody', { eventEntry: this.props.events[this.props.eventId].eventEntry, qoins: this.props.qoins })} />
+                <ConfirmationDialog
+                    visible={this.state.openUserDontHaveEnoughQoinsDialog}
+                    closeModal={this.toggleUserDontHaveEnoughQoinsDialog}
+                    cancelButton={false}
+                    accept={this.cancelRegistration}
+                    body={translate('eventDetailsModal.notEnoughQoinsDialogBody', { eventEntry: this.props.events[this.props.eventId].eventEntry, qoins: this.props.qoins })} />
             </Modal>
         );
     }
@@ -105,7 +195,9 @@ class EventDetailsModal extends Component {
 function mapStateToProps(state) {
     return {
         events: state.logrosReducer,
-        games: state.gamesReducer.games
+        games: state.gamesReducer.games,
+        uid: state.userReducer.user.id,
+        qoins: state.userReducer.user.credits
     }
 }
 

622d098ddc300611ece27e10f1c04849a1e61c9b
DHVSGG <diego@qapla.gg>
2021-05-06 18:14:46 -0500

    getTwitchUserName function added

--- src/services/database.js
+++ src/services/database.js
@@ -166,26 +166,20 @@ export async function createUserProfile(Uid, email, userName ) {
     checkNotificationPermission(Uid);
 }
 
-
 /**
- * Check if the given twitchId is already used by any user
- * @param {string} twitchId Twitch identifier
+ * Returns the string value of the Twitch userName of the given user
+ * @param {string} uid User identifier
  */
- export async function userHaveTwitchId2(uid) {
-
-    return (await usersRef.child(uid).child('twitchId').once('value')).exists();
-    //return (await usersRef.child(uid).child('twitchId').once('value')).val(); 
+export async function getTwitchUserName(uid) {
+    return (await usersRef.child(uid).child('twitchUsername').once('value')).val();
 }
-/**
- * Check if the given twitchId is already used by any user
- * @param {string} twitchId Twitch identifier
- */
-export async function userHaveTwitchId(uid) {
-    //const userTwitchID = usersRef.child(uid).child('twitchId').once('value');
-    
-    const userTwitchLink = (await usersRef.child(uid).child('twitchId').once('value'));
 
-    return userTwitchLink
+/**
+ * Check if the user has their twitchId on their profile (if we have their twitch account linked)
+ * @param {string} uid User identifier
+ */
+ export async function userHaveTwitchId(uid) {
+    return (await usersRef.child(uid).child('twitchId').once('value')).exists();
 }
 
 /**

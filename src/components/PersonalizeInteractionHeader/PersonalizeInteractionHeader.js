aa72299ceffda8084faa6d6c85c447d1ad66e9a5
DHVSGG <diego@qapla.gg>
2022-07-11 17:19:33 -0500

    White statusbar bug fixed

--- src/components/PersonalizeInteractionHeader/PersonalizeInteractionHeader.js
+++ src/components/PersonalizeInteractionHeader/PersonalizeInteractionHeader.js
@@ -8,32 +8,30 @@ import images from '../../../assets/images';
 class PersonalizeInteractionHeader extends Component {
     render() {
         return (
-            <SafeAreaView>
-                <View style={styles.container}>
-                    <View style={styles.mainContainer}>
-                        {this.props.currentScreen !== 'InteractionsSent' &&
-                            <>
-                                {this.props.currentScreen !== 'InteractionsCheckout' &&
-                                <TouchableOpacity
-                                    style={styles.backButtonContainer}
-                                    onPress={() => this.props.navigation.pop()}>
-                                    <images.svg.leftArrowThiccIcon />
-                                </TouchableOpacity>}
-                                <View style={styles.streamerContainer}>
-                                    <Image
-                                        source={{ uri: this.props.navigation.getParam('photoUrl') }}
-                                        style={styles.streamerImage}
-                                    />
-                                    <Text style={styles.streamerName}>
-                                        {this.props.navigation.getParam('displayName', '')}
-                                    </Text>
-                                    {this.props.navigation.getParam('isStreaming', false) &&
-                                        <View style={styles.streamerLive} />
-                                    }
-                                </View>
-                            </>
-                        }
-                    </View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.mainContainer}>
+                    {this.props.currentScreen !== 'InteractionsSent' &&
+                        <>
+                            {this.props.currentScreen !== 'InteractionsCheckout' &&
+                            <TouchableOpacity
+                                style={styles.backButtonContainer}
+                                onPress={() => this.props.navigation.pop()}>
+                                <images.svg.leftArrowThiccIcon />
+                            </TouchableOpacity>}
+                            <View style={styles.streamerContainer}>
+                                <Image
+                                    source={{ uri: this.props.navigation.getParam('photoUrl') }}
+                                    style={styles.streamerImage}
+                                />
+                                <Text style={styles.streamerName}>
+                                    {this.props.navigation.getParam('displayName', '')}
+                                </Text>
+                                {this.props.navigation.getParam('isStreaming', false) &&
+                                    <View style={styles.streamerLive} />
+                                }
+                            </View>
+                        </>
+                    }
                 </View>
             </SafeAreaView>
         );

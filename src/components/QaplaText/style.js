05f4aabe1a8699b45194eb888b1a629cb2a7a1fc
DHVSGG <diego@qapla.gg>
2022-08-27 16:06:29 -0500

    Inconsistent font on iOS removed

--- src/components/QaplaText/style.js
+++ src/components/QaplaText/style.js
@@ -2,7 +2,5 @@ import { StyleSheet } from 'react-native';
 
 export default styles = StyleSheet.create({
     qaplaBaseText: {
-        fontFamily: 'SFRounded-Ultralight',
-        fontWeight: 'bold'
     }
 });

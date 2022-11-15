62c1c08db9e39921f070c28545f0ac20f6391b10
DHVSGG <diego@qapla.gg>
2022-11-15 17:40:29 -0600

    Handle sign up on reactions type screen

--- src/screens/ReactionTypesScreen/ReactionTypesScreen.js
+++ src/screens/ReactionTypesScreen/ReactionTypesScreen.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 
+import SignUpModal from '../../components/SignUpModal/SignUpModal';
+import { retrieveData } from '../../utilities/persistance';
 import styles from './style';
 
 class ReactionTypesScreen extends Component {
+    state = {
+        openSignUpModal: false,
+        reactionLevel: 1
+    };
+
+    goToReaction = async (reactionLevel) => {
+        if (this.props.uid) {
+            this.props.navigation.navigate('TweetReactionScreen', { reactionLevel });
+        } else {
+            const freeReactionsSent = await retrieveData('freeReactionsSent');
+            if (freeReactionsSent) {
+                this.setState({ openSignUpModal: true, reactionLevel });
+            }
+        }
+    }
+
+    onSignUpSuccess = () => {
+        this.setState({ openSignUpModal: false });
+        this.props.navigation.navigate('TweetReactionScreen', { reactionLevel: this.state.reactionLevel });
+    }
+
     render() {
         return (
             <SafeAreaView style={styles.container}>
@@ -11,7 +35,7 @@ class ReactionTypesScreen extends Component {
                         marginTop: 16,
                         backgroundColor: '#FFF'
                     }}
-                    onPress={() => this.props.navigation.navigate('TweetReactionScreen', { reactionLevel: 1 })}>
+                    onPress={() => this.goToReaction(1)}>
                     <Text>
                         Nivel 1
                     </Text>
@@ -20,7 +44,7 @@ class ReactionTypesScreen extends Component {
                         marginTop: 16,
                         backgroundColor: '#FFF'
                     }}
-                    onPress={() => this.props.navigation.navigate('TweetReactionScreen', { reactionLevel: 2 })}>
+                    onPress={() => this.goToReaction(2)}>
                     <Text>
                         Nivel 2
                     </Text>
@@ -29,14 +53,29 @@ class ReactionTypesScreen extends Component {
                         marginTop: 16,
                         backgroundColor: '#FFF'
                     }}
-                    onPress={() => this.props.navigation.navigate('TweetReactionScreen', { reactionLevel: 3 })}>
+                    onPress={() => this.goToReaction(3)}>
                     <Text>
                         Nivel 3
                     </Text>
                 </TouchableOpacity>
+                <SignUpModal open={this.state.openSignUpModal}
+                    onClose={() => this.setState({ openSignUpModal: false })}
+                    title='Keep reacting on stream'
+                    benefits={[
+                        '⚡️ Use channel points to send custom memes',
+                        '🔥 Upgrade your memes',
+                        '🌱 Support your fave streamers'
+                    ]}
+                    onSignUpSuccess={this.onSignUpSuccess} />
             </SafeAreaView>
         );
     }
 }
 
-export default ReactionTypesScreen;
+function mapStateToProps(state) {
+    return {
+        uid: state.userReducer.user.id
+    };
+}
+
+export default connect(mapStateToProps)(ReactionTypesScreen);
